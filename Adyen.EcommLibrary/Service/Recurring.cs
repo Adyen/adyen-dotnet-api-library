2e27d945ce8fd27384565b21d6351917d67cc678
alexandros <alexandros.moraitis@adyen.com>
2017-11-14 16:42:46 +0100

    Fix recurring

--- Adyen.EcommLibrary/Service/Recurring.cs
+++ Adyen.EcommLibrary/Service/Recurring.cs
@@ -1,15 +1,35 @@
-﻿using Adyen.EcommLibrary.Service.Resource.Reccuring;
+﻿using Adyen.EcommLibrary.Model.Reccuring;
+using Adyen.EcommLibrary.Service.Resource.Reccuring;
+using System;
 
 namespace Adyen.EcommLibrary.Service
 {
-    public class Recurring:AbstractService
+    public class Recurring : AbstractService
     {
-        private ListReccuringDetails _listRecurringDetails;
+        private readonly ListReccuringDetails _listRecurringDetails;
         private Disable _disable;
 
         public Recurring(Client client) : base(client)
         {
-            _listRecurringDetails=new ListReccuringDetails(this);
+            _listRecurringDetails = new ListReccuringDetails(this);
+            _disable = new Disable(this);
+
+        }
+
+        public RecurringDetailsResult ListRecurringDetails(RecurringDetailsRequest request)
+        {
+            RecurringDetailsResult result = null;
+            try
+            {
+                var jsonRequest = Util.JsonOperation.SerializeRequest(request);
+                var jsonResponse = _listRecurringDetails.Request(jsonRequest);
+                result = Util.JsonOperation.Deserealize<RecurringDetailsResult>(jsonResponse);
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+            return result;
 
         }
     }
