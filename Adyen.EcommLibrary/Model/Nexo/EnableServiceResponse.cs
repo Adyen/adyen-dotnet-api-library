d641d32d3c6e0718d9c999650159c4876cd3a80d
alexandros <alexandros.moraitis@adyen.com>
2018-07-18 10:35:27 +0200

    Add interface IMessagePayload for serialization to request - response models

--- Adyen.EcommLibrary/Model/Nexo/EnableServiceResponse.cs
+++ Adyen.EcommLibrary/Model/Nexo/EnableServiceResponse.cs
@@ -1,11 +1,13 @@
-﻿namespace Adyen.EcommLibrary.Model.Nexo
+﻿using Adyen.EcommLibrary.CloudApiSerialization;
+
+namespace Adyen.EcommLibrary.Model.Nexo
 {
     /// <remarks/>
     [System.CodeDom.Compiler.GeneratedCodeAttribute("xsd", "4.6.1055.0")]
     [System.SerializableAttribute()]
     [System.Diagnostics.DebuggerStepThroughAttribute()]
     [System.ComponentModel.DesignerCategoryAttribute("code")]
-    public partial class EnableServiceResponse
+    public partial class EnableServiceResponse : IMessagePayload
     {
 
         /// <remarks/>
--- Adyen.EcommLibrary/Model/Nexo/LoyaltyResponse.cs
+++ Adyen.EcommLibrary/Model/Nexo/LoyaltyResponse.cs
@@ -1,11 +1,13 @@
-﻿namespace Adyen.EcommLibrary.Model.Nexo
+﻿using Adyen.EcommLibrary.CloudApiSerialization;
+
+namespace Adyen.EcommLibrary.Model.Nexo
 {
     /// <remarks/>
     [System.CodeDom.Compiler.GeneratedCodeAttribute("xsd", "4.6.1055.0")]
     [System.SerializableAttribute()]
     [System.Diagnostics.DebuggerStepThroughAttribute()]
     [System.ComponentModel.DesignerCategoryAttribute("code")]
-    public partial class LoyaltyResponse
+    public partial class LoyaltyResponse : IMessagePayload
     {
 
         /// <remarks/>
