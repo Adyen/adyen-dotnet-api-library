f067df248f396ee5a3761c472a24bc161132a3c2
alexandros <alexandros.moraitis@adyen.com>
2018-11-23 10:31:13 +0100

    remove doublicate tests in open invoice

--- Adyen.EcommLibrary.Test/PaymentTest.cs
+++ Adyen.EcommLibrary.Test/PaymentTest.cs
@@ -70,20 +70,16 @@ namespace Adyen.EcommLibrary.Test
             var paymentResult = CreatePaymentResultFromFile("Mocks/authorise-success-cse.json");
             Assert.AreEqual(ResultCodeEnum.Authorised, paymentResult.ResultCode);
         }
-        
+
         [TestMethod]
         public void TestOpenInvoice()
         {
-
             var client = CreateMockTestClientRequest("Mocks/authorise-success-klarna.json");
             var payment = new Payment(client);
             var paymentRequest = MockOpenInvoicePayment.CreateOpenInvoicePaymentRequest();
-            var paymentResult = payment.Authorise(paymentRequest);           
+            var paymentResult = payment.Authorise(paymentRequest);
             Assert.AreEqual("2374421290", paymentResult.AdditionalData["additionalData.acquirerReference"]);
             Assert.AreEqual("klarna", paymentResult.AdditionalData["paymentMethodVariant"]);
-            Assert.IsNotNull(paymentRequest.ApplicationInfo);
-            Assert.AreEqual(paymentRequest.ApplicationInfo.AdyenLibrary.Name,ClientConfig.LibName);
-            Assert.AreEqual(paymentRequest.ApplicationInfo.AdyenLibrary.Version,ClientConfig.LibVersion);
         }
 
         [TestMethod]
