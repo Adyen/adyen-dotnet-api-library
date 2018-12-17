b90de1bedd4ad0f94f82926f6b5a02d29fcfe2d8
alexandros <alexandros.moraitis@adyen.com>
2018-12-17 14:25:08 +0100

    update version

--- Adyen.EcommLibrary/Constants/ClientConfig.cs
+++ Adyen.EcommLibrary/Constants/ClientConfig.cs
@@ -19,6 +19,6 @@
         public static string EndpointProtocol = "https://";
 
         public static string LibName = "adyen-dotnet-api-library";
-        public static string LibVersion = "1.1.2";
+        public static string LibVersion = "1.1.3";
     }
 }
