53dbfa236c78c9cf497b502fc1d99188a6264d8d
alexandros <alexandros.moraitis@adyen.com>
2019-01-28 14:27:53 +0100

    Add json response to http client exception

--- Adyen.EcommLibrary/HttpClient/HttpClientException.cs
+++ Adyen.EcommLibrary/HttpClient/HttpClientException.cs
@@ -7,10 +7,11 @@ namespace Adyen.EcommLibrary.HttpClient
     public class HttpClientException : Exception
     {
         public int Code { get; private set; }
-        public   WebHeaderCollection WebHeaderCollection { get; private set; }
+        public string JsonResponse { get; private set; }
+        public WebHeaderCollection WebHeaderCollection { get; private set; }
         public string ResponseBody { get; private set; }
-        
-        public HttpClientException( int code, string message,  WebHeaderCollection  webHeaderCollection, string responseBody) : base(message)
+
+        public HttpClientException(int code, string message, WebHeaderCollection webHeaderCollection, string responseBody) : base(message)
         {
             this.Code = code;
             this.WebHeaderCollection = webHeaderCollection;
