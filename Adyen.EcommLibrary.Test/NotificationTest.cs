87f2cb7552a91cbe4b9a45a6df129c991a3103e9
Guilherme Branco Stracini <guilherme@guilhermebranco.com.br>
2019-05-07 08:47:55 -0300

    Fix the mock file using the @AlexandroMor review suggestion

--- Adyen.EcommLibrary.Test/NotificationTest.cs
+++ Adyen.EcommLibrary.Test/NotificationTest.cs
@@ -10,9 +10,8 @@
         [TestMethod]
         public void TestAuthorisationSuccess()
         {
-            //var jsonRequest = base.MockFileToString("Mocks/Notification/authorisation-true.json");
-            var jsonRequest = "{\"live\":\"false\",\"notificationItems\":[{\"NotificationRequestItem\":{\"additionalData\":{\"expiryDate\":\"12/2012\",\" NAME1 \":\"VALUE1\",\"authCode\":\"1234\",\"cardSummary\":\"7777\",\"totalFraudScore\":\"10\",\"hmacSignature\":\"OzDjCMZIsdtDqrZ+cl/FWC+WdESrorctXTzAzW33dXI=\",\"NAME2\":\"  VALUE2  \",\"fraudCheck-6-ShopperIpUsage\":\"10\"},\"amount\":{\"currency\":\"EUR\",\"value\":10100},\"eventCode\":\"AUTHORISATION\",\"eventDate\":\"2017-01-19T16:42:03+01:00\",\"merchantAccountCode\":\"MagentoMerchantTest2\",\"merchantReference\":\"8313842560770001\",\"operations\":[\"CANCEL\",\"CAPTURE\",\"REFUND\"],\"paymentMethod\":\"visa\",\"pspReference\":\"123456789\",\"reason\":\"1234:7777:12/2012\",\"success\":\"true\"}}]}";
-            var notificationHandler = new NotificationHandler();
+            var mockPath = GetMockFilePath("Mocks/notification/authorisation-true.json");
+            var jsonRequest = MockFileToString(mockPath); var notificationHandler = new NotificationHandler();
             var handleNotificationRequest = notificationHandler.HandleNotificationRequest(jsonRequest);
             var notificationRequestItemContainer = handleNotificationRequest.NotificationItemContainers.FirstOrDefault();
             if (notificationRequestItemContainer == null)
@@ -26,8 +25,8 @@
         [TestMethod]
         public void TestCaptureSuccess()
         {
-            //var jsonRequest = base.MockFileToString("Mocks/Notification/capture-true.json");
-            var jsonRequest = "{\"live\":\"false\",\"notificationItems\":[{\"NotificationRequestItem\":{\"additionalData\":{\"hmacSignature\":\"qvS6I3Gdi1jx+jSh7IopAgcHtMoxvXlNL7DYQ+j1hd0=\"},\"amount\":{\"currency\":\"USD\",\"value\":23623},\"eventCode\":\"CAPTURE\",\"eventDate\":\"2017-01-25T18:08:19+01:00\",\"merchantAccountCode\":\"MagentoMerchantTest2\",\"merchantReference\":\"00000001\",\"originalReference\":\"ORIGINAL_PSP\",\"paymentMethod\":\"visa\",\"pspReference\":\"PSP_REFERENCE\",\"reason\":\"\",\"success\":\"true\"}}]}";
+            var mockPath = GetMockFilePath("Mocks/notification/capture-true.json");
+            var jsonRequest = MockFileToString(mockPath);
             var notificationHandler = new NotificationHandler();
             var handleNotificationRequest = notificationHandler.HandleNotificationRequest(jsonRequest);
             var notificationRequestItemContainer = handleNotificationRequest.NotificationItemContainers.FirstOrDefault();
@@ -44,8 +43,8 @@
         [TestMethod]
         public void TestCaptureFail()
         {
-            //var jsonRequest = base.MockFileToString("Mocks/Notification/capture-false.json");
-            var jsonRequest = "{\"live\":\"false\",\"notificationItems\":[{\"NotificationRequestItem\":{\"additionalData\":{\"hmacSignature\":\"KujHNqpyCAMdGefj7lfQ8AeD0Jke9Zs2bVAqScQDWi4=\"},\"amount\":{\"currency\":\"USD\",\"value\":23623},\"eventCode\":\"CAPTURE\",\"eventDate\":\"2017-01-25T18:08:19+01:00\",\"merchantAccountCode\":\"MagentoMerchantTest2\",\"merchantReference\":\"00000001\",\"originalReference\":\"ORIGINAL_PSP\",\"paymentMethod\":\"visa\",\"pspReference\":\"PSP_REFERENCE\",\"reason\":\"Insufficient balance on payment\",\"success\":\"false\"}}]}";
+            var mockPath = GetMockFilePath("Mocks/notification/capture-false.json");
+            var jsonRequest = MockFileToString(mockPath);
             var notificationHandler = new NotificationHandler();
             var handleNotificationRequest = notificationHandler.HandleNotificationRequest(jsonRequest);
             var notificationRequestItemContainer = handleNotificationRequest.NotificationItemContainers.FirstOrDefault();
@@ -63,8 +62,8 @@
         [TestMethod]
         public void TestRefundSuccess()
         {
-            //var jsonRequest = base.MockFileToString("Mocks/Notification/refund-true.json");
-            var jsonRequest = "{\"live\":\"false\",\"notificationItems\":[{\"NotificationRequestItem\":{\"additionalData\":{\"hmacSignature\":\"KJFhURWP8Pv9m8k+7NGHNJAupBj6X6J/VWAikFxeWhA=\"},\"amount\":{\"currency\":\"EUR\",\"value\":1500},\"eventCode\":\"REFUND\",\"eventDate\":\"2017-02-17T11:11:44+01:00\",\"merchantAccountCode\":\"MagentoMerchantTest2\",\"merchantReference\":\"payment-2017-1-17-11-refund\",\"originalReference\":\"ORIGINAL_PSP\",\"paymentMethod\":\"visa\",\"pspReference\":\"PSP_REFERENCE\",\"reason\":\"\",\"success\":\"true\"}}]}";
+            var mockPath = GetMockFilePath("Mocks/notification/refund-true.json");
+            var jsonRequest = MockFileToString(mockPath);
             var notificationHandler = new NotificationHandler();
             var handleNotificationRequest = notificationHandler.HandleNotificationRequest(jsonRequest);
             var notificationRequestItemContainer = handleNotificationRequest.NotificationItemContainers.FirstOrDefault();
@@ -82,8 +81,8 @@
         [TestMethod]
         public void TestRefundFail()
         {
-            //var jsonRequest = base.MockFileToString("Mocks/Notification/refund-false.json");
-            var jsonRequest = "{\"live\":\"false\",\"notificationItems\":[{\"NotificationRequestItem\":{\"additionalData\":{\"hmacSignature\":\"HZXziBYopfDIzDhk49iC//yCfxmy/z0xWuvvTxFNUSA=\"},\"amount\":{\"currency\":\"EUR\",\"value\":1500},\"eventCode\":\"REFUND\",\"eventDate\":\"2017-02-17T11:04:07+01:00\",\"merchantAccountCode\":\"MagentoMerchantTest2\",\"merchantReference\":\"payment-2017-1-17-11-refund\",\"originalReference\":\"ORIGINAL_PSP\",\"paymentMethod\":\"visa\",\"pspReference\":\"PSP_REFERENCE\",\"reason\":\"Insufficient balance on payment\",\"success\":\"false\"}}]}";
+            var mockPath = GetMockFilePath("Mocks/notification/refund-false.json");
+            var jsonRequest = MockFileToString(mockPath);
             var notificationHandler = new NotificationHandler();
             var handleNotificationRequest = notificationHandler.HandleNotificationRequest(jsonRequest);
             var notificationRequestItemContainer = handleNotificationRequest.NotificationItemContainers.FirstOrDefault();
