bc1fc66a4d9cbc68ab7c2c76ab21429e1a42d2d6
alexandros <alexandros.moraitis@adyen.com>
2019-06-20 14:17:48 +0200

    DisplayResponse

--- Adyen/Model/Nexo/DisplayResponse.cs
+++ Adyen/Model/Nexo/DisplayResponse.cs
@@ -1,13 +1,14 @@
-﻿namespace Adyen.Model.Nexo
+﻿using Adyen.CloudApiSerialization;
+
+namespace Adyen.Model.Nexo
 {
     /// <remarks/>
     [System.CodeDom.Compiler.GeneratedCodeAttribute("xsd", "4.6.1055.0")]
     [System.SerializableAttribute()]
     [System.Diagnostics.DebuggerStepThroughAttribute()]
     [System.ComponentModel.DesignerCategoryAttribute("code")]
-    public partial class DisplayResponse
+    public partial class DisplayResponse : IMessagePayload
     {
-
         /// <remarks/>
         [System.Xml.Serialization.XmlElementAttribute("OutputResult", Form = System.Xml.Schema.XmlSchemaForm.Unqualified)]
         public OutputResult[] OutputResult;
