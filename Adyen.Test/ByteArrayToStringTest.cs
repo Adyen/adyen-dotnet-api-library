b224d5c8dbeb0414d1979b42684eebc4e0f048ce
alexandros <alexandros.moraitis@adyen.com>
2019-06-24 15:40:06 -0700

    Test byte array serialization deserialization

---
+++ Adyen.Test/ByteArrayToStringTest.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Adyen.Model;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Adyen.Test
+{
+    [TestClass]
+
+    public class ByteArrayToStringTest
+    {
+        [TestMethod]
+        public void TestSerialize()
+        {
+            var json = "{\"cavv\":\"Qnl0ZXMtVG8tQmUtRW5jb2RlZA==\",\"xid\":\"Qnl0ZXMtVG8tQmUtRW5jb2RlZA==\"}";
+            var bytesArray = Convert.FromBase64String("Qnl0ZXMtVG8tQmUtRW5jb2RlZA==");
+            var threeDSecure = new ThreeDSecureData
+            {
+                Cavv = bytesArray,
+                Xid = bytesArray
+            };
+            var jsonRequest = Util.JsonOperation.SerializeRequest(threeDSecure);
+            Assert.AreEqual(json, jsonRequest);
+        }
+        
+        [TestMethod]
+        public void TestDeserialize()
+        {
+            var json = "{\"cavv\":\"Qnl0ZXMtVG8tQmUtRW5jb2RlZA==\",\"xid\":\"Qnl0ZXMtVG8tQmUtRW5jb2RlZA==\"}";
+            var jsonElementBase64 = "Qnl0ZXMtVG8tQmUtRW5jb2RlZA==";
+            var jsonRequest = Util.JsonOperation.Deserialize<ThreeDSecureData>(json);
+            var xid = Convert.ToBase64String(jsonRequest.Xid);
+            var cavv = Convert.ToBase64String(jsonRequest.Cavv);
+            Assert.AreEqual(xid,jsonElementBase64);
+            Assert.AreEqual(cavv, jsonElementBase64);
+        }
+    }
+}
