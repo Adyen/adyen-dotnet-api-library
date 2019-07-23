1da018acdec42649db0a9311a1dfd221ff39dc6d
alexandros <alexandros.moraitis@adyen.com>
2019-07-23 13:18:31 +0200

    Add Serializer test class

---
+++ Adyen.Test/SerializerTest.cs
@@ -0,0 +1,30 @@
+﻿using Adyen.CloudApiSerialization;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Adyen.Model.Nexo;
+
+namespace Adyen.Test
+{
+    [TestClass]
+    public class SerializerTest
+    {
+        //OnLine is with capital L
+        private readonly string _message1 = "{\"SaleToPOIResponse\": {\"PaymentResponse\": {\"POIData\": {},\"PaymentResult\": {\"AuthenticationMethod\": [\"OnLinePIN\"],\"PaymentAcquirerData\": {\"AcquirerPOIID\": \"MX925-260390740\",\"MerchantID\": \"PME_POS\"},\"PaymentType\": \"Normal\"},\"Response\": {\"Result\": \"Success\"}},\"MessageHeader\": {\"ProtocolVersion\": \"3.0\",\"SaleID\": \"Appie\",\"MessageClass\": \"Service\",\"MessageCategory\": \"Payment\",\"ServiceID\": \"20095135\",\"POIID\": \"MX925-260390740\",\"MessageType\": \"Response\"}}}";
+        //OnLine is with lower case L
+        private readonly string _message2 = "{\"SaleToPOIResponse\": {\"PaymentResponse\": {\"POIData\": {},\"PaymentResult\": {\"AuthenticationMethod\": [\"OnlinePIN\"],\"PaymentAcquirerData\": {\"AcquirerPOIID\": \"MX925-260390740\",\"MerchantID\": \"PME_POS\"},\"PaymentType\": \"Normal\"},\"Response\": {\"Result\": \"Success\"}},\"MessageHeader\": {\"ProtocolVersion\": \"3.0\",\"SaleID\": \"Appie\",\"MessageClass\": \"Service\",\"MessageCategory\": \"Payment\",\"ServiceID\": \"20095135\",\"POIID\": \"MX925-260390740\",\"MessageType\": \"Response\"}}}";
+
+        [TestMethod]
+        public void EnumSerializerTest()
+        {
+            var saleToPoiMessageSerializer = new SaleToPoiMessageSerializer();
+            var saleToMessageOnLine = saleToPoiMessageSerializer.Deserialize(_message1);
+            var saleToMessageOnline = saleToPoiMessageSerializer.Deserialize(_message2);
+            var paymentResponseOnLine = (PaymentResponse)saleToMessageOnLine.MessagePayload;
+            var paymentResponseOnline = (PaymentResponse)saleToMessageOnline.MessagePayload;
+            Assert.AreEqual(paymentResponseOnline.PaymentResult.AuthenticationMethod[0], AuthenticationMethodType.OnLinePIN);
+            Assert.AreEqual(paymentResponseOnLine.PaymentResult.AuthenticationMethod[0], AuthenticationMethodType.OnLinePIN);
+        }
+    }
+}
