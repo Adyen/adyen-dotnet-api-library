0bf044023fa4a77d25fd4bfa722a7a3a7f74d95d
alexandros <alexandros.moraitis@adyen.com>
2019-07-23 13:27:37 +0200

    refactor serializer test class

--- Adyen.Test/SerializerTest.cs
+++ Adyen.Test/SerializerTest.cs
@@ -10,21 +10,21 @@ namespace Adyen.Test
     [TestClass]
     public class SerializerTest
     {
-        //OnLine is with capital L
-        private readonly string _message1 = "{\"SaleToPOIResponse\": {\"PaymentResponse\": {\"POIData\": {},\"PaymentResult\": {\"AuthenticationMethod\": [\"OnLinePIN\"],\"PaymentAcquirerData\": {\"AcquirerPOIID\": \"MX925-260390740\",\"MerchantID\": \"PME_POS\"},\"PaymentType\": \"Normal\"},\"Response\": {\"Result\": \"Success\"}},\"MessageHeader\": {\"ProtocolVersion\": \"3.0\",\"SaleID\": \"Appie\",\"MessageClass\": \"Service\",\"MessageCategory\": \"Payment\",\"ServiceID\": \"20095135\",\"POIID\": \"MX925-260390740\",\"MessageType\": \"Response\"}}}";
-        //OnLine is with lower case L
-        private readonly string _message2 = "{\"SaleToPOIResponse\": {\"PaymentResponse\": {\"POIData\": {},\"PaymentResult\": {\"AuthenticationMethod\": [\"OnlinePIN\"],\"PaymentAcquirerData\": {\"AcquirerPOIID\": \"MX925-260390740\",\"MerchantID\": \"PME_POS\"},\"PaymentType\": \"Normal\"},\"Response\": {\"Result\": \"Success\"}},\"MessageHeader\": {\"ProtocolVersion\": \"3.0\",\"SaleID\": \"Appie\",\"MessageClass\": \"Service\",\"MessageCategory\": \"Payment\",\"ServiceID\": \"20095135\",\"POIID\": \"MX925-260390740\",\"MessageType\": \"Response\"}}}";
-
         [TestMethod]
         public void EnumSerializerTest()
         {
             var saleToPoiMessageSerializer = new SaleToPoiMessageSerializer();
-            var saleToMessageOnLine = saleToPoiMessageSerializer.Deserialize(_message1);
-            var saleToMessageOnline = saleToPoiMessageSerializer.Deserialize(_message2);
+            var saleToMessageOnLine = saleToPoiMessageSerializer.Deserialize(GetSaleToPoiMessage("OnlinePin"));
+            var saleToMessageOnline = saleToPoiMessageSerializer.Deserialize(GetSaleToPoiMessage("OnLinePin"));
             var paymentResponseOnLine = (PaymentResponse)saleToMessageOnLine.MessagePayload;
             var paymentResponseOnline = (PaymentResponse)saleToMessageOnline.MessagePayload;
             Assert.AreEqual(paymentResponseOnline.PaymentResult.AuthenticationMethod[0], AuthenticationMethodType.OnLinePIN);
             Assert.AreEqual(paymentResponseOnLine.PaymentResult.AuthenticationMethod[0], AuthenticationMethodType.OnLinePIN);
+        }
+
+        private static string GetSaleToPoiMessage(string online)
+        {
+            return "{\"SaleToPOIResponse\": {\"PaymentResponse\": {\"POIData\": {},\"PaymentResult\": {\"AuthenticationMethod\": [\""+ online+"\"],\"PaymentAcquirerData\": {\"AcquirerPOIID\": \"MX925-260390740\",\"MerchantID\": \"PME_POS\"},\"PaymentType\": \"Normal\"},\"Response\": {\"Result\": \"Success\"}},\"MessageHeader\": {\"ProtocolVersion\": \"3.0\",\"SaleID\": \"Appie\",\"MessageClass\": \"Service\",\"MessageCategory\": \"Payment\",\"ServiceID\": \"20095135\",\"POIID\": \"MX925-260390740\",\"MessageType\": \"Response\"}}}";
         }
     }
 }
