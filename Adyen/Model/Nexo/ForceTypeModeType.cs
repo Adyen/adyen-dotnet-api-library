2c3a8cf0845982fff62024a8f28f3274a497418e
alexandros <alexandros.moraitis@adyen.com>
2019-07-24 14:51:50 +0200

    remove comment lines

--- Adyen/Model/Nexo/ForceTypeModeType.cs
+++ Adyen/Model/Nexo/ForceTypeModeType.cs
@@ -6,34 +6,24 @@
     public enum ForceEntryModeType
     {
         //Payment instrument information are taken from RFID
-        /// <remarks/>
         RFID,
         //Manual key entry
-        /// <remarks/>
         Keyed,
         //Reading of embossing or OCR of printed data either at time of transaction or after the event.
-        /// <remarks/>
         Manual,
         //Account data on file
-        /// <remarks/>
         File,
         //Scanned by a bar code reader.
-        /// <remarks/>
         Scanned,
         //Magnetic stripe
-        /// <remarks/>
         MagStripe,
         //Contact ICC (asynchronous)
-        /// <remarks/>
         ICC,
         //Contact ICC (synchronous)
-        /// <remarks/>
         SynchronousICC,
         //Contactless card reader Magnetic Stripe
-        /// <remarks/>
         Tapped,
         //Contactless card reader conform to ISO 14443        
-        /// <remarks/>
         Contactless,
     }
 }
