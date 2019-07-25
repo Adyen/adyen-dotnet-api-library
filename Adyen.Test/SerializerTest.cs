8e48d15f36b9711cfcc8c227a1866be497232f51
alexandros <alexandros.moraitis@adyen.com>
2019-07-25 11:32:18 +0200

    Add ByteArraySerializer

--- Adyen.Test/SerializerTest.cs
+++ Adyen.Test/SerializerTest.cs
@@ -1,15 +1,48 @@
 ﻿using Adyen.CloudApiSerialization;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
-using System;
-using System.Collections.Generic;
-using System.Text;
 using Adyen.Model.Nexo;
+using System.Text;
+using Adyen.Model;
 
 namespace Adyen.Test
 {
     [TestClass]
     public class SerializerTest
     {
+        [TestMethod]
+        public void SerializeByteArrayTest()
+        {
+            var plainTextBytes = Encoding.ASCII.GetBytes("Bytes-To-Be-Encoded");
+            string base64String = System.Convert.ToBase64String(plainTextBytes);
+            var base64Bytes = Encoding.ASCII.GetBytes(base64String);
+            var threeDSecure = new ThreeDSecureData
+            {
+                Cavv = base64Bytes,
+                Xid = base64Bytes
+            };
+            var jsonRequest = Util.JsonOperation.SerializeRequest(threeDSecure);
+            var json = "{\"cavv\":\"Qnl0ZXMtVG8tQmUtRW5jb2RlZA==\",\"xid\":\"Qnl0ZXMtVG8tQmUtRW5jb2RlZA==\"}";
+            Assert.AreEqual(json, jsonRequest);
+        }
+
+        private static string GetSaleToPoiMessage(string online)
+        {
+            return "{\"SaleToPOIResponse\": {\"PaymentResponse\": {\"POIData\": {},\"PaymentResult\": {\"AuthenticationMethod\": [\"" + online + "\"],\"PaymentAcquirerData\": {\"AcquirerPOIID\": \"MX925-260390740\",\"MerchantID\": \"PME_POS\"},\"PaymentType\": \"Normal\"},\"Response\": {\"Result\": \"Success\"}},\"MessageHeader\": {\"ProtocolVersion\": \"3.0\",\"SaleID\": \"Appie\",\"MessageClass\": \"Service\",\"MessageCategory\": \"Payment\",\"ServiceID\": \"20095135\",\"POIID\": \"MX925-260390740\",\"MessageType\": \"Response\"}}}";
+        }
+
+        [TestMethod]
+        public void DeserializeByteArrayTest()
+        {
+            var json = "{\"cavv\":\"Qnl0ZXMtVG8tQmUtRW5jb2RlZA==\",\"xid\":\"Qnl0ZXMtVG8tQmUtRW5jb2RlZA==\"}";
+            var jsonRequest = Util.JsonOperation.Deserialize<ThreeDSecureData>(json);
+            var xid = Encoding.ASCII.GetString(jsonRequest.Xid);
+            var cavv = Encoding.ASCII.GetString(jsonRequest.Cavv);
+
+            var jsonElementBase64 = "Qnl0ZXMtVG8tQmUtRW5jb2RlZA==";
+            Assert.AreEqual(jsonElementBase64, xid);
+            Assert.AreEqual(jsonElementBase64, cavv);
+        }
+
         [TestMethod]
         public void EnumSerializerTest()
         {
@@ -20,11 +53,6 @@ namespace Adyen.Test
             var paymentResponseOnline = (PaymentResponse)saleToMessageOnline.MessagePayload;
             Assert.AreEqual(paymentResponseOnline.PaymentResult.AuthenticationMethod[0], AuthenticationMethodType.OnLinePIN);
             Assert.AreEqual(paymentResponseOnLine.PaymentResult.AuthenticationMethod[0], AuthenticationMethodType.OnLinePIN);
-        }
-
-        private static string GetSaleToPoiMessage(string online)
-        {
-            return "{\"SaleToPOIResponse\": {\"PaymentResponse\": {\"POIData\": {},\"PaymentResult\": {\"AuthenticationMethod\": [\""+ online+"\"],\"PaymentAcquirerData\": {\"AcquirerPOIID\": \"MX925-260390740\",\"MerchantID\": \"PME_POS\"},\"PaymentType\": \"Normal\"},\"Response\": {\"Result\": \"Success\"}},\"MessageHeader\": {\"ProtocolVersion\": \"3.0\",\"SaleID\": \"Appie\",\"MessageClass\": \"Service\",\"MessageCategory\": \"Payment\",\"ServiceID\": \"20095135\",\"POIID\": \"MX925-260390740\",\"MessageType\": \"Response\"}}}";
         }
     }
 }
