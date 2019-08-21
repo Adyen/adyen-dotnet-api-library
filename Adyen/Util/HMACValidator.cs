68205ee2d3f4ac9bcc0bef0e3002c91142a8756b
Robert ten Bosch <12122234+robertftenbosch@users.noreply.github.com>
2019-08-21 16:37:26 +0200

    Update HMACValidator.cs
    notificationRequestItem Success is a boolean and will become True or False starting with uppercase.
    While in the json is  success:true of success:false with lowercase.
    this results in a different hash. and an false IsValidHmac
    this is something I ran in to while parsing the jsonrequest in the NotificationHandler.HandleNotificationRequest(jsonRequest).
    maybe this is an error on my part, but I thought I should let you know and don't know if the proposed ToLower is the best solution?

--- Adyen/Util/HMACValidator.cs
+++ Adyen/Util/HMACValidator.cs
@@ -95,7 +95,7 @@ namespace Adyen.Util
                 Convert.ToString(amount.Value),
                 amount.Currency,
                 notificationRequestItem.EventCode,
-                notificationRequestItem.Success.ToString()
+                notificationRequestItem.Success.ToString().ToLower()
             };
             return String.Join(":", signedDataList);
         }
