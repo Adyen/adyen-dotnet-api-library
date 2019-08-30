d34719f7a4af8bfbcaf32d266d0f920dea793e58
alexandros <alexandros.moraitis@adyen.com>
2019-08-30 15:31:24 +0200

    Update teh version 3.3.0

--- Adyen/Constants/ClientConfig.cs
+++ Adyen/Constants/ClientConfig.cs
@@ -45,6 +45,6 @@ namespace Adyen.Constants
         public static string BinLookupApiVersion = "v50";
 
         public static string LibName = "adyen-dotnet-api-library";
-        public static string LibVersion = "3.2.3";
+        public static string LibVersion = "3.3.0";
     }
 }
