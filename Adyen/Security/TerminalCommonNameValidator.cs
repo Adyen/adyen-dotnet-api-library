2a619584e1bedca0612ff49bc9f14cba49639ecc
alexandros <alexandros.moraitis@adyen.com>
2019-10-21 14:20:13 +0200

    Correct environment typo

--- Adyen/Security/TerminalCommonNameValidator.cs
+++ Adyen/Security/TerminalCommonNameValidator.cs
@@ -28,27 +28,27 @@ namespace Adyen.Security
 {
     public static class TerminalCommonNameValidator
     {
-        private static readonly string _enviromentWildcard = "{enviroment}";
-        private static string _terminalApiCnRegex = "[a-zA-Z0-9]{4,}-[0-9]{9}\\." + _enviromentWildcard + "\\.terminal\\.adyen\\.com";
-        private static string _terminalApiLegacy = "legacy-terminal-certificate." + _enviromentWildcard + ".terminal.adyen.com";
+        private static readonly string _environmentWildcard = "{enviroment}";
+        private static string _terminalApiCnRegex = "[a-zA-Z0-9]{4,}-[0-9]{9}\\." + _environmentWildcard + "\\.terminal\\.adyen\\.com";
+        private static string _terminalApiLegacy = "legacy-terminal-certificate." + _environmentWildcard + ".terminal.adyen.com";
 
         public static bool ValidateCertificate(string certificateSubject, Model.Enum.Environment environment)
-        {       var enviromentName = environment.ToString().ToLower();
-                var regexPatternTerminalSpecificCert = _terminalApiCnRegex.Replace(_enviromentWildcard, enviromentName);
-                var regexPatternLegacyCert = _terminalApiLegacy.Replace(_enviromentWildcard, enviromentName);
-                var subject = certificateSubject.Split(',')
-                         .Select(x => x.Split('='))
-                         .ToDictionary(x => x[0].Trim(' '), x => x[1]);
-                if(subject.ContainsKey("CN"))
+        {
+            var environmentName = environment.ToString().ToLower();
+            var regexPatternTerminalSpecificCert = _terminalApiCnRegex.Replace(_environmentWildcard, environmentName);
+            var regexPatternLegacyCert = _terminalApiLegacy.Replace(_environmentWildcard, environmentName);
+            var subject = certificateSubject.Split(',')
+                     .Select(x => x.Split('='))
+                     .ToDictionary(x => x[0].Trim(' '), x => x[1]);
+            if (subject.ContainsKey("CN"))
+            {
+                string commonNameValue = subject["CN"];
+                if (Regex.Match(commonNameValue, regexPatternTerminalSpecificCert).Success || string.Equals(commonNameValue, regexPatternLegacyCert))
                 {
-                    string commonNameValue = subject["CN"];
-                    if (Regex.Match(commonNameValue, regexPatternTerminalSpecificCert).Success || string.Equals(commonNameValue, regexPatternLegacyCert))
-                    {
-                        return true;
-                    }
+                    return true;
                 }
-                return false;
-         
+            }
+            return false;
         }
     }
 }
