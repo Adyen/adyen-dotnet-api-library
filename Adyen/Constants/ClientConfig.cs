fefd051341df841bc47391a4ac93c5d2aec09f91
alexandros <alexandros.moraitis@adyen.com>
2020-01-31 15:55:53 +0100

    udpate version to 5.1.0

--- Adyen/Constants/ClientConfig.cs
+++ Adyen/Constants/ClientConfig.cs
@@ -49,6 +49,6 @@ namespace Adyen.Constants
         public static string BinLookupApiVersion = "v50";
 
         public static string LibName = "adyen-dotnet-api-library";
-        public static string LibVersion = "5.0.0";
+        public static string LibVersion = "5.1.0";
     }
 }
