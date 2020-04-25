f5008cad9190d3936f4352761cc23c12e5017f6d
alexandros <alexandros.moraitis@adyen.com>
2020-04-25 16:50:38 +0200

    Fix all the warnings

--- Adyen/Model/Checkout/Split.cs
+++ Adyen/Model/Checkout/Split.cs
@@ -217,8 +217,7 @@ namespace Adyen.Model.Checkout
                 ) && 
                 (
                     this.Type == input.Type ||
-                    (this.Type != null &&
-                    this.Type.Equals(input.Type))
+                    (this.Type.Equals(input.Type))
                 );
         }
 
@@ -239,8 +238,7 @@ namespace Adyen.Model.Checkout
                     hashCode = hashCode * 59 + this.Description.GetHashCode();
                 if (this.Reference != null)
                     hashCode = hashCode * 59 + this.Reference.GetHashCode();
-                if (this.Type != null)
-                    hashCode = hashCode * 59 + this.Type.GetHashCode();
+                hashCode = hashCode * 59 + this.Type.GetHashCode();
                 return hashCode;
             }
         }
