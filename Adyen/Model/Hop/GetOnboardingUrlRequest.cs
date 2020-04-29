f7802ac73f1765caf35f59a4df4998d75ab8b8b1
Alexandros Moraitis <amoraitis@outlook.com>
2020-04-29 09:19:47 +0200

    Update Adyen/Model/Hop/GetOnboardingUrlRequest.cs

--- Adyen/Model/Hop/GetOnboardingUrlRequest.cs
+++ Adyen/Model/Hop/GetOnboardingUrlRequest.cs
@@ -51,6 +51,26 @@ namespace Adyen.Model.Hop
     [DataMember(Name="platformName", EmitDefaultValue=false)]
     [JsonProperty(PropertyName = "platformName")]
     public string PlatformName { get; set; }
-        public string HopWebserviceUser { get; set; }
+     /// <summary>
+    /// Get the string presentation of the object
+    /// </summary>
+    /// <returns>String presentation of the object</returns>
+    public override string ToString()  {
+      var sb = new StringBuilder();
+      sb.Append("class GetOnboardingUrlRequest {\n");
+      sb.Append("  AccountHolderCode: ").Append(AccountHolderCode).Append("\n");
+      sb.Append("  PlatformName: ").Append(PlatformName).Append("\n");
+      sb.Append("  ReturnUrl: ").Append(ReturnUrl).Append("\n");
+      sb.Append("}\n");
+      return sb.ToString();
+    }
+
+    /// <summary>
+    /// Get the JSON string presentation of the object
+    /// </summary>
+    /// <returns>JSON string presentation of the object</returns>
+    public string ToJson() {
+      return JsonConvert.SerializeObject(this, Formatting.Indented);
+    }
     }
 }
