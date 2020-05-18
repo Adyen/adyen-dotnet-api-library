e38807ebee27976729f041d94e80a634971de699
Julius Bartkus <julius@viggo.com>
2020-05-18 18:52:32 +0300

    Feat(idempotency-key): allow modifications to pass idempotency key

--- Adyen/Service/Modification.cs
+++ Adyen/Service/Modification.cs
@@ -21,6 +21,7 @@
 //  */
 #endregion
 
+using Adyen.Model;
 using Adyen.Model.Modification;
 using Adyen.Service.Resource.Modification;
 
@@ -35,8 +36,8 @@ namespace Adyen.Service
         private readonly AdjustAuthorisation _adjustAuthorisation;
         private readonly VoidPendingRefund _voidPendingRefund;
 
-        public Modification(Client clinet)
-            : base(clinet)
+        public Modification(Client client)
+            : base(client)
         {
             _capture = new Capture(this);
             _cancelOrRefund = new CancelOrRefund(this);
@@ -46,55 +47,50 @@ namespace Adyen.Service
             _voidPendingRefund = new VoidPendingRefund(this);
         }
         
-        public ModificationResult Capture(CaptureRequest request)
+        public ModificationResult Capture(CaptureRequest request, RequestOptions requestOptions = null)
         {
             var jsonRequest = Util.JsonOperation.SerializeRequest(request);
-            var jsonResult = _capture.Request(jsonRequest);
+            var jsonResult = _capture.Request(jsonRequest, requestOptions);
 
             return Util.JsonOperation.Deserialize<ModificationResult>(jsonResult);
         }
         
-        public ModificationResult CancelOrRefund(CancelOrRefundRequest request)
+        public ModificationResult CancelOrRefund(CancelOrRefundRequest request, RequestOptions requestOptions = null)
         {
             var jsonRequest = Util.JsonOperation.SerializeRequest(request);
-            var jsonResult = _cancelOrRefund.Request(jsonRequest);
+            var jsonResult = _cancelOrRefund.Request(jsonRequest, requestOptions);
 
             return Util.JsonOperation.Deserialize<ModificationResult>(jsonResult);
         }
         
-        public ModificationResult Refund(RefundRequest request)
+        public ModificationResult Refund(RefundRequest request, RequestOptions requestOptions = null)
         {
             var jsonRequest = Util.JsonOperation.SerializeRequest(request);
-            var jsonResult = _refund.Request(jsonRequest);
+            var jsonResult = _refund.Request(jsonRequest, requestOptions);
 
             return Util.JsonOperation.Deserialize<ModificationResult>(jsonResult);
         }
         
-        public ModificationResult Cancel(CancelRequest request)
+        public ModificationResult Cancel(CancelRequest request, RequestOptions requestOptions = null)
         {
             var jsonRequest = Util.JsonOperation.SerializeRequest(request);
-            var jsonResult = _cancel.Request(jsonRequest);
+            var jsonResult = _cancel.Request(jsonRequest, requestOptions);
 
             return Util.JsonOperation.Deserialize<ModificationResult>(jsonResult);
         }
 
-        public ModificationResult AdjustAuthorisation(AdjustAuthorisationRequest request)
-        {
-            var jsonRequest = Util.JsonOperation.SerializeRequest(request);
-            var jsonResult = _adjustAuthorisation.Request(jsonRequest);
-
-            return Util.JsonOperation.Deserialize<ModificationResult>(jsonResult);
-        }
-
-        public ModificationResult VoidPendingRefund(VoidPendingRefundRequest request)
-        {
-            return VoidPendingRefund(request, null);
-        }
-
-        public ModificationResult VoidPendingRefund(VoidPendingRefundRequest request, Model.RequestOptions requestOptions )
+        public ModificationResult AdjustAuthorisation(AdjustAuthorisationRequest request, RequestOptions requestOptions = null)
         {
             var jsonRequest = Util.JsonOperation.SerializeRequest(request);
             var jsonResult = _adjustAuthorisation.Request(jsonRequest, requestOptions);
+
+            return Util.JsonOperation.Deserialize<ModificationResult>(jsonResult);
+        }
+
+        public ModificationResult VoidPendingRefund(VoidPendingRefundRequest request, RequestOptions requestOptions = null)
+        {
+            var jsonRequest = Util.JsonOperation.SerializeRequest(request);
+            var jsonResult = _voidPendingRefund.Request(jsonRequest, requestOptions);
 
             return Util.JsonOperation.Deserialize<ModificationResult>(jsonResult);
         }
