a88db1230fab164a138cb5d955d730faa7474a67
karlkaal <karl@frontsystems.no>
2020-06-26 09:20:05 +0200

    Added GetTerminalDetails endpoint

---
+++ Adyen/Model/PosTerminalManagement/GetTerminalDetailsRequest.cs
@@ -0,0 +1,44 @@
+﻿using Newtonsoft.Json;
+using System.Runtime.Serialization;
+using System.Text;
+
+namespace Adyen.Model.PosTerminalManagement
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    [DataContract]
+    public class GetTerminalDetailsRequest
+    {
+        /// <summary>
+        /// The unique terminal ID in the format `[Device model]-[Serial number]`.   For example, **V400m-324689776**.
+        /// </summary>
+        /// <value>The unique terminal ID in the format `[Device model]-[Serial number]`.   For example, **V400m-324689776**.</value>
+        [DataMember(Name = "terminal", EmitDefaultValue = false)]
+        [JsonProperty(PropertyName = "terminal")]
+        public string Terminal { get; set; }
+
+
+        /// <summary>
+        /// Get the string presentation of the object
+        /// </summary>
+        /// <returns>String presentation of the object</returns>
+        public override string ToString()
+        {
+            var sb = new StringBuilder();
+            sb.Append($"class {nameof(GetTerminalDetailsRequest)} {{\n");
+            sb.Append("  Terminal: ").Append(Terminal).Append("\n");
+            sb.Append("}\n");
+            return sb.ToString();
+        }
+
+        /// <summary>
+        /// Get the JSON string presentation of the object
+        /// </summary>
+        /// <returns>JSON string presentation of the object</returns>
+        public string ToJson()
+        {
+            return JsonConvert.SerializeObject(this, Formatting.Indented);
+        }
+    }
+}
---
+++ Adyen/Model/PosTerminalManagement/GetTerminalDetailsResponse.cs
@@ -0,0 +1,42 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Runtime.Serialization;
+using System.Text;
+
+namespace Adyen.Model.PosTerminalManagement
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    [DataContract]
+    public class GetTerminalDetailsResponse : Dictionary<string, object>
+    {
+        /// <summary>
+        /// Get the string presentation of the object
+        /// </summary>
+        /// <returns>String presentation of the object</returns>
+        public override string ToString()
+        {
+            var sb = new StringBuilder();
+            sb.Append($"class {nameof(GetTerminalDetailsResponse)} {{\n");
+
+            foreach (var kvp in this)
+            {
+              sb.Append($"  {kvp.Key}: {kvp.Value}\n");
+            }
+
+            sb.Append("}\n");
+            return sb.ToString();
+        }
+
+        /// <summary>
+        /// Get the JSON string presentation of the object
+        /// </summary>
+        /// <returns>JSON string presentation of the object</returns>
+        public string ToJson()
+        {
+            return JsonConvert.SerializeObject(this, Formatting.Indented);
+        }
+    }
+}
