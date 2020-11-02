a8e27d285b279f3ed09f2c835ce412b490725c69
James Simm <james.simm@gmail.com>
2020-11-02 13:43:04 +0000

    Adds test to confirm can deserialize paymentmethod.

---
+++ Adyen.Test/ApplePayDeserializationTest.cs
@@ -0,0 +1,23 @@
+﻿using Adyen.Model.Checkout;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Newtonsoft.Json;
+
+namespace Adyen.Test
+{
+    [TestClass]
+    public class ApplePayDeserializationTest : BaseTest
+    {
+        [TestMethod]
+        public void Can_deserialize_applepay_paymentmethod_request()
+        {
+            var mockPath = GetMockFilePath("Mocks/checkout/paymentmethod-applepay-request.json");
+            var response = MockFileToString(mockPath);
+
+            var paymentMethod = JsonConvert.DeserializeObject<DefaultPaymentMethodDetails>(response);
+
+            Assert.IsNotNull(paymentMethod);
+            Assert.AreEqual("applepay", paymentMethod.Type);
+            Assert.AreEqual("VNRWtuNlNEWkRCSm1xWndjMDFFbktkQU...", paymentMethod.ApplePayToken);
+        }
+    }
+}
