9201b1f8ffc2f557083663bbb423eec79a208999
Alexandros Moraitis <amoraitis@outlook.com>
2021-01-07 22:13:54 +0100

    Bump the version 6.0.0

--- Adyen/Constants/ClientConfig.cs
+++ Adyen/Constants/ClientConfig.cs
@@ -53,6 +53,6 @@ namespace Adyen.Constants
         public static string BinLookupApiVersion = "v50";
 
         public static string LibName = "adyen-dotnet-api-library";
-        public static string LibVersion = "5.7.0";
+        public static string LibVersion = "6.0.0";
     }
 }
