9f9177ed09f31aa336919c0ec44af204816b3ad1
alexandros <alexandros.moraitis@adyen.com>
2022-06-27 14:50:13 +0200

    Add the e2e tests

---
+++ Adyen.IntegrationTest/ClientConstants.cs
@@ -0,0 +1,14 @@
+﻿
+using System;
+
+namespace Adyen.IntegrationTest
+{
+    public class ClientConstants
+    {
+        public static readonly string MerchantAccount = Environment.GetEnvironmentVariable("INTEGRATION_MERCHANT_ACCOUNT");
+        public static readonly string Xapikey = Environment.GetEnvironmentVariable("INTEGRATION_X_API_KEY");
+        
+        public static readonly string CaUsername = Environment.GetEnvironmentVariable("INTEGRATION_CA_USERNAME");
+        public static readonly string CaPassword = Environment.GetEnvironmentVariable("INTEGRATION_CA_PASSWORD");
+    }
+}
---
+++ Adyen.IntegrationTest/NotificationTest.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Adyen.IntegrationTest
+{
+    [TestClass]
+    public class NotificationTest
+    {
+        //[TestMethod]
+        //public void TestAuthorisationSuccess()
+        //{
+
+        //}
+
+        //[TestMethod]
+        //public void TestCaptureSuccess()
+        //{
+
+        //}
+
+        //[TestMethod]
+        //public void TestCaptureFail()
+        //{
+
+        //}
+
+        //[TestMethod]
+        //public void TestRefundSuccess()
+        //{
+
+        //}
+
+        //[TestMethod]
+        //public void TestRefundFail()
+        //{
+
+        //}
+    }
+}
