dc5ad0a4ad468f38a0c4418b1aaaaf9842a7b0f8
jillingk <jilling.kingma@adyen.com>
2022-11-10 13:56:36 +0100

    FiXIntEgrAtiOntEsT

--- Adyen.IntegrationTest/PayoutTest.cs
+++ Adyen.IntegrationTest/PayoutTest.cs
@@ -26,7 +26,7 @@ namespace Adyen.IntegrationTest
         {
             PayoutRequest payoutRequest = CreatePayoutRequest("DotNetAlexandros");
             PayoutResponse result = _payout.PayoutSubmit(payoutRequest);
-            Assert.AreEqual(result.ResultCode, "Received");
+            Assert.AreEqual(result.ResultCode, "Authorised");
         }
 
 
