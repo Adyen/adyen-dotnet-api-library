ec5709ae183546ee5c385d5a516f6257b40831ca
jillingk <93914435+jillingk@users.noreply.github.com>
2022-11-21 11:09:39 +0100

    test with dummy CA key

--- Adyen.IntegrationTest/ClientConstants.cs
+++ Adyen.IntegrationTest/ClientConstants.cs
@@ -7,7 +7,7 @@ namespace Adyen.IntegrationTest
         public static readonly string MerchantAccount = Environment.GetEnvironmentVariable("INTEGRATION_MERCHANT_ACCOUNT");
 
         public static readonly string Xapikey =
-            "12345ldskajg;lkj939692j;fdkgja;elrkjt2384568594302fdsadg;ktrjetr23798559684329345fdsa";
+            "AQEwhmfxJo3GbBdFw0m/n3Q5qf3VfI5eGbBFVXVXyGH6zDXzQ826g1ALHYQE3/a+9EmJEMFdWw2+5HzctViMSCJMYAc=-lmQ8ow42JTlz9E7yeMv7YorGjOrGZJYb8KGxyIPto8I=->y<*juIuEG:t9^>9";
         
         public static readonly string CaUsername = Environment.GetEnvironmentVariable("INTEGRATION_CA_USERNAME");
         public static readonly string CaPassword = Environment.GetEnvironmentVariable("INTEGRATION_CA_PASSWORD");
