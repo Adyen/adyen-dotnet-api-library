76f5e4a37cbd5d7edf68f9ab887eda2044b1f904
jillingk <93914435+jillingk@users.noreply.github.com>
2023-01-09 14:05:26 +0100

    Changed services

--- Adyen/Service/Payout.cs
+++ Adyen/Service/Payout.cs
@@ -1,4 +1,6 @@
-﻿using Adyen.Model.Payout;
+﻿using System.Threading.Tasks;
+using Adyen.Model;
+using Adyen.Model.Payout;
 using Adyen.Service.Resource.Payout;
 using Newtonsoft.Json;
 
@@ -6,12 +8,12 @@ namespace Adyen.Service
 {
     public class Payout : AbstractService
     {
-        private StoreDetailAndSubmitThirdParty _storeDetailAndSubmitThirdParty;
-        private ConfirmThirdParty _confirmThirdParty;
-        private DeclineThirdParty _declineThirdParty;
-        private StoreDetail _storeDetail;
-        private SubmitThirdParty _submitThirdParty;
-        private PayoutService _payoutService;
+        private readonly StoreDetailAndSubmitThirdParty _storeDetailAndSubmitThirdParty;
+        private readonly ConfirmThirdParty _confirmThirdParty;
+        private readonly DeclineThirdParty _declineThirdParty;
+        private readonly StoreDetail _storeDetail;
+        private readonly SubmitThirdParty _submitThirdParty;
+        private readonly PayoutService _payoutService;
 
         public Payout(Client client)
             : base(client)
@@ -24,46 +26,149 @@ namespace Adyen.Service
             _payoutService = new PayoutService(this);
         }
 
-        public StoreDetailAndSubmitResponse StoreDetailAndSubmitThirdParty(StoreDetailAndSubmitRequest request)
+        /// <summary>
+        /// Post storeDetailAndSubmitThirdPartyAsync API call 
+        /// </summary>
+        /// <param name="storeDetailAndSubmitRequest"></param>
+        /// <param name="requestOptions"></param>
+        /// <returns>PaymentsResponse</returns>
+        public async Task<StoreDetailAndSubmitResponse> StoreDetailAndSubmitThirdPartyAsync(
+            StoreDetailAndSubmitRequest storeDetailAndSubmitRequest, RequestOptions requestOptions = null)
         {
-            var jsonRequest = Util.JsonOperation.SerializeRequest(request);
-            var jsonResponse = _storeDetailAndSubmitThirdParty.Request(jsonRequest);
+            var jsonRequest = storeDetailAndSubmitRequest.ToJson();
+            var jsonResponse = await _storeDetailAndSubmitThirdParty.RequestAsync(jsonRequest, requestOptions);
             return JsonConvert.DeserializeObject<StoreDetailAndSubmitResponse>(jsonResponse);
         }
-
-        public ConfirmThirdPartyResponse ConfirmThirdParty(ConfirmThirdPartyRequest request)
+        
+        /// <summary>
+        /// Post storeDetailAndSubmitThirdParty API call 
+        /// </summary>
+        /// <param name="storeDetailAndSubmitRequest"></param>
+        /// <param name="requestOptions"></param>
+        /// <returns>PaymentsResponse</returns>
+        public StoreDetailAndSubmitResponse StoreDetailAndSubmitThirdParty(StoreDetailAndSubmitRequest storeDetailAndSubmitRequest, RequestOptions requestOptions = null)
         {
-            var jsonRequest = Util.JsonOperation.SerializeRequest(request);
-            var jsonResponse = _confirmThirdParty.Request(jsonRequest);
-            return JsonConvert.DeserializeObject<ConfirmThirdPartyResponse>(jsonResponse);
+            return StoreDetailAndSubmitThirdPartyAsync(storeDetailAndSubmitRequest, requestOptions).GetAwaiter().GetResult();
         }
         
-        public DeclineThirdPartyResponse DeclineThirdParty(DeclineThirdPartyRequest request)
+        /// <summary>
+        /// Post ConfirmThirdPartyAsync API call 
+        /// </summary>
+        /// <param name="modifyRequest"></param>
+        /// <param name="requestOptions"></param>
+        /// <returns>PaymentsResponse</returns>
+        public async Task<ModifyResponse> ConfirmThirdPartyAsync(ModifyRequest modifyRequest, RequestOptions requestOptions = null)
         {
-            var jsonRequest = Util.JsonOperation.SerializeRequest(request);
-            var jsonResponse = _declineThirdParty.Request(jsonRequest);
-            return JsonConvert.DeserializeObject<DeclineThirdPartyResponse>(jsonResponse);
+            var jsonRequest = modifyRequest.ToJson();
+            var jsonResponse = await _confirmThirdParty.RequestAsync(jsonRequest, requestOptions);
+            return JsonConvert.DeserializeObject<ModifyResponse>(jsonResponse);
         }
 
-        public StoreDetailResponse StoreDetail(StoreDetailRequest request)
+        /// <summary>
+        /// Post ConfirmThirdParty API call 
+        /// </summary>
+        /// <param name="modifyRequest"></param>
+        /// <param name="requestOptions"></param>
+        /// <returns>PaymentsResponse</returns>
+        public ModifyResponse ConfirmThirdParty(ModifyRequest modifyRequest, RequestOptions requestOptions = null)
         {
-            var jsonRequest = Util.JsonOperation.SerializeRequest(request);
-            var jsonResponse = _storeDetail.Request(jsonRequest);
+            return ConfirmThirdPartyAsync(modifyRequest, requestOptions).GetAwaiter().GetResult();
+        }
+        
+        /// <summary>
+        /// Post DeclineThirdPartyAsync API call 
+        /// </summary>
+        /// <param name="modifyRequest"></param>
+        /// <param name="requestOptions"></param>
+        /// <returns>PaymentsResponse</returns>
+        public async Task<ModifyResponse> DeclineThirdPartyAsync(ModifyRequest modifyRequest, RequestOptions requestOptions = null)
+        {
+            var jsonRequest = modifyRequest.ToJson();
+            var jsonResponse = await _declineThirdParty.RequestAsync(jsonRequest, requestOptions);
+            return JsonConvert.DeserializeObject<ModifyResponse>(jsonResponse);
+        }
+
+        /// <summary>
+        /// Post DeclineThirdParty API call 
+        /// </summary>
+        /// <param name="modifyRequest"></param>
+        /// <param name="requestOptions"></param>
+        /// <returns>PaymentsResponse</returns>
+        public ModifyResponse DeclineThirdParty(ModifyRequest modifyRequest, RequestOptions requestOptions = null)
+        {
+            return DeclineThirdPartyAsync(modifyRequest, requestOptions).GetAwaiter().GetResult();
+        }
+        
+        /// <summary>
+        /// Post StoreDetailAsync API call 
+        /// </summary>
+        /// <param name="storeDetailRequest"></param>
+        /// <param name="requestOptions"></param>
+        /// <returns>PaymentsResponse</returns>
+        public async Task<StoreDetailResponse> StoreDetailAsync(StoreDetailRequest storeDetailRequest, RequestOptions requestOptions = null)
+        {
+            var jsonRequest = storeDetailRequest.ToJson();
+            var jsonResponse = await _storeDetail.RequestAsync(jsonRequest, requestOptions);
             return JsonConvert.DeserializeObject<StoreDetailResponse>(jsonResponse);
         }
-
-        public SubmitResponse SubmitThirdParty(SubmitRequest request)
+        
+        /// <summary>
+        /// Post StoreDetail API call 
+        /// </summary>
+        /// <param name="storeDetailRequest"></param>
+        /// <param name="requestOptions"></param>
+        /// <returns>PaymentsResponse</returns>
+        public StoreDetailResponse StoreDetail(StoreDetailRequest storeDetailRequest, RequestOptions requestOptions = null)
         {
-            var jsonRequest = Util.JsonOperation.SerializeRequest(request);
-            var jsonResponse = _submitThirdParty.Request(jsonRequest);
+            return StoreDetailAsync(storeDetailRequest, requestOptions).GetAwaiter().GetResult();
+        }
+
+        /// <summary>
+        /// Post SubmitThirdPartyAsync API call 
+        /// </summary>
+        /// <param name="submitRequest"></param>
+        /// <param name="requestOptions"></param>
+        /// <returns>PaymentsResponse</returns>
+        public async Task<SubmitResponse> SubmitThirdPartyAsync(SubmitRequest submitRequest, RequestOptions requestOptions = null)
+        {
+            var jsonRequest = submitRequest.ToJson();
+            var jsonResponse = await _submitThirdParty.RequestAsync(jsonRequest, requestOptions);
             return JsonConvert.DeserializeObject<SubmitResponse>(jsonResponse);
         }
 
-        public PayoutResponse PayoutSubmit(PayoutRequest request)
+        /// <summary>
+        /// Post SubmitThirdParty API call 
+        /// </summary>
+        /// <param name="submitRequest"></param>
+        /// <param name="requestOptions"></param>
+        /// <returns>PaymentsResponse</returns>
+        public SubmitResponse SubmitThirdParty(SubmitRequest submitRequest, RequestOptions requestOptions = null)
         {
-            var jsonRequest = Util.JsonOperation.SerializeRequest(request);
-            var jsonResponse = _payoutService.Request(jsonRequest);
+            return SubmitThirdPartyAsync(submitRequest, requestOptions).GetAwaiter().GetResult();
+        }
+
+        /// <summary>
+        /// Post PayoutSubmitAsync API call 
+        /// </summary>
+        /// <param name="payoutRequest"></param>
+        /// <param name="requestOptions"></param>
+        /// <returns>PaymentsResponse</returns>
+        public async Task<PayoutResponse> PayoutSubmitAsync(PayoutRequest payoutRequest, RequestOptions requestOptions = null)
+        {
+            var jsonRequest = payoutRequest.ToJson();
+            var jsonResponse = await _payoutService.RequestAsync(jsonRequest, requestOptions);
             return JsonConvert.DeserializeObject<PayoutResponse>(jsonResponse);
+        }
+
+        /// <summary>
+        /// Post PayoutSubmit API call 
+        /// </summary>
+        /// <param name="payoutRequest"></param>
+        /// <param name="requestOptions"></param>
+        /// <returns>PaymentsResponse</returns>
+        public PayoutResponse PayoutSubmit(PayoutRequest payoutRequest, RequestOptions requestOptions = null)
+        {
+            return PayoutSubmitAsync(payoutRequest, requestOptions).GetAwaiter().GetResult();
         }
     }
 }
