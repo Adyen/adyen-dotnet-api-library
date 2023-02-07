a476f792610aac4ea7f8ff8edf58cabb3cb44e95
jillingk <93914435+jillingk@users.noreply.github.com>
2023-02-07 14:24:48 +0100

    leaked creds..

--- Adyen.IntegrationTest/ClientConstants.cs
+++ Adyen.IntegrationTest/ClientConstants.cs
@@ -4,8 +4,8 @@ namespace Adyen.IntegrationTest
 {
     public class ClientConstants
     {
-        public static readonly string MerchantAccount = "TestMerchantAccount";
-        public static readonly string Xapikey = "AQEwhmfxJo3GbBdFw0m/n3Q5qf3VfI5eGbBFVXVXyGH6zDXzQ826g1ALHYQE3/a+9EmJEMFdWw2+5HzctViMSCJMYAc=-ZFf0nHHORUtjUQE9OQj5+RXxuYWdLNT505GcBq+BJbI=-u^?sZ>Cm$2B.gYBW";
+        public static readonly string MerchantAccount = Environment.GetEnvironmentVariable("INTEGRATION_MERCHANT_ACCOUNT");
+        public static readonly string Xapikey = Environment.GetEnvironmentVariable("INTEGRATION_X_API_KEY");
         
         public static readonly string CaUsername = Environment.GetEnvironmentVariable("INTEGRATION_CA_USERNAME");
         public static readonly string CaPassword = Environment.GetEnvironmentVariable("INTEGRATION_CA_PASSWORD");
