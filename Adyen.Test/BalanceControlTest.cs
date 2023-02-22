6e18f891122d4cfdf425705f43ff626abf4aa56c
jillingk <93914435+jillingk@users.noreply.github.com>
2023-02-22 09:51:30 +0100

    Generated Service/Model and add small UT

---
+++ Adyen.Test/BalanceControlTest.cs
@@ -0,0 +1,21 @@
+using System;
+using Adyen.Model.BalanceControl;
+using Adyen.Service.BalanceControl;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Adyen.Test
+{
+    [TestClass]
+    public class BalanceControlTest : BaseTest
+    {
+        [TestMethod]
+        public void BalanceTransferTest()
+        {
+            var client = CreateMockTestClientApiKeyBasedRequestAsync("Mocks/balance-control-transfer.json");
+            var service = new BalanceControlService(client);
+            var response = service.StartBalanceTransfer(new BalanceTransferRequest());
+            Assert.AreEqual(response.CreatedAt, new DateTime(2022,01, 24, 14,59, 11));
+            Assert.AreEqual(response.Status, BalanceTransferResponse.StatusEnum.Transferred);
+        }
+    }
+}
--- Makefile
+++ Makefile
@@ -12,6 +12,9 @@ output:=target/out
 # Generate models (for each service)
 models: $(services)
 
+BalanceControl: spec=BalanceControlService-v1
+BalanceControl: service=balanceControlService
+BalancePlatform: spec=BalancePlatformService-v2
 BinLookup: spec=BinLookupService-v52
 Checkout: spec=CheckoutService-v70
 Checkout: service=checkout
@@ -23,7 +26,6 @@ Payouts: spec=PayoutService-v68
 Management: spec=ManagementService-v1
 Management: service=management
 LegalEntityManagement: spec=LegalEntityService-v2
-BalancePlatform: spec=BalancePlatformService-v2
 PlatformsAccount: spec=AccountService-v6
 PlatformsFund: spec=FundService-v6
 PlatformsNotificationConfiguration: spec=NotificationConfigurationService-v6
