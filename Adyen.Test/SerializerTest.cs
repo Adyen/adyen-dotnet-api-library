7bf873610acd096adf5105c9aabe56aa16595a44
jillingk <93914435+jillingk@users.noreply.github.com>
2023-04-06 17:42:47 +0200

    [DR-182] Adjust templates to fix incorrect Long (de)serialization (#776)
    * Adjust templates to fix incorrect long (de)serialization
    
    * add serialization test for long zero values
    
    * use isNumeric flag to catch all numerical values and remove method preventing serialization for readonly flags
    
    * small mistake
    
    * fix
    
    * add extra Unit Test for readonly parameter

--- Adyen.Test/SerializerTest.cs
+++ Adyen.Test/SerializerTest.cs
@@ -1,8 +1,12 @@
-﻿using System.Text;
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
 using Adyen.ApiSerialization;
 using Adyen.Model.Checkout;
 using Adyen.Model.Nexo;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
 using PaymentResponse = Adyen.Model.Nexo.PaymentResponse;
 
 namespace Adyen.Test
@@ -48,6 +52,39 @@ namespace Adyen.Test
             var paymentResponseOnline = (PaymentResponse)saleToMessageOnline.MessagePayload;
             Assert.AreEqual(paymentResponseOnline.PaymentResult.AuthenticationMethod[0], AuthenticationMethodType.OnLinePIN);
             Assert.AreEqual(paymentResponseOnLine.PaymentResult.AuthenticationMethod[0], AuthenticationMethodType.OnLinePIN);
+        }
+
+        [TestMethod]
+        public void CheckoutLongSerializationTest()
+        {
+            var checkoutSessionRequest = new CreateCheckoutSessionRequest
+            {
+                MerchantAccount = "merchantAccount",
+                Reference = "TestReference",
+                ReturnUrl = "http://test-url.com",
+                Amount = new Model.Checkout.Amount("EUR", 10000L),
+                Channel = CreateCheckoutSessionRequest.ChannelEnum.Web,
+                CountryCode = "NL",
+                LineItems = new List<LineItem>()
+                {
+                    new LineItem(quantity: 1, amountIncludingTax: 5000, description: "description1",
+                        amountExcludingTax: 0),
+                    new LineItem(quantity: 1, amountIncludingTax: 5000, description: "description2", taxAmount: 0)
+                }
+            }.ToJson();
+            // Assert that Long parameters set to zero are actually serialised (just like Int)
+            Assert.IsTrue(checkoutSessionRequest.Contains("amountExcludingTax\": 0,"));
+            Assert.IsTrue(checkoutSessionRequest.Contains("taxAmount\": 0"));
+        }
+        
+        [TestMethod]
+        public void CheckoutSessionResponseCheckForIdTest()
+        {
+            var sessionResponse = @"{""mode"": ""embedded"",""amount"": {""currency"": ""EUR"",""value"": 10000},""expiresAt"": ""2023-04-06T17:11:15+02:00"",""id"": ""CS0068299CB8DA273A"",""merchantAccount"": ""TestMerchantAccount"",""reference"": ""TestReference"",""returnUrl"": ""http://test-url.com"",""sessionData"": ""Ab02b4c0!BQABAgBoacRJuRbNM/typUKATopkZ3V+cINm0WTAvwl9uQJ2e8I00P2KFemlwp4nb1bOqqYh1zx48gDAHt0QDs2JTiQIqDQRizqopLFRk/wAJHFoCuam/GvOHflg9vwS3caHbkBToIolxcYcJoJheIN9o1fGmNIHZb9VrWfiKsXMgmYsSUifenayS2tkKCTquF7vguaAwk7q5ES1GDwzP/J2mChJGH04jGrVL4szPHGmznr897wIcFQyBSZe4Uifqoe0fpiIxZLNWadLMya6SnvQYNAQL1V6ti+7F4wHHyLwHWTCua993sttue70TE4918EV80HcAWx0LE1+uW6J5KBHCKdYNi9ESlGSZreRwLCpdNbmumB6MlyHZlz2umLiw0ZZJAEpdrwXA2NiyHzJDKDKfbAhd8uoTSACrbgwbrAXI1cvb1WjiOQjLn9MVW++fuJCq6vIeX+rUKMeltBAHMeBZyC54ndAucw9nS03uyckjphunE4tl4WTT475VkfUiyJCTT3S2IOVYS9V9M8pMQ1/GlDVNCLBJJfrYlVXoC8VX68QW1HERkhNYQbTgLQ9kI3cDeMP5d4TuUL3XR2Q6sNiOMdIPGDYQ9QFKFdOE3OQ5X9wxUYbX6j88wR2gRGJcS5agqFHOR1ZTNrjumOYrEkjL8ehFXEs/KluhURllfi0POUPGAwlUWBjDCZcKaeeR0kASnsia2V5IjoiQUYwQUFBMTAzQ0E1MzdFQUVEODdDMjRERDUzOTA5QjgwQTc4QTkyM0UzODIzRDY4REFDQzk0QjlGRjgzMDVEQyJ9E0Gs1T0RaO7NluuXP59fegcW6SQKq4BhC3ZzEKPm1vJuwAJ2gZUaicaXbRPW3IyadavKRlfGdFeAYt2B3ik8fGiK3ZkKU0CrZ0qL0IO5rrWrOg74HMnpxRAn1RhAHRHfGEk67FFPNjr0aLBJXSia7xZWiyKA+i4QU63roY2sxMI/q41mvJjRUz0rPKT3SbVDt1Of7K7BP8cmiboBkWexFBD/mkJyMOpYAOoFp/tKOUHTWZYcc1GpbyV1jInXVfEUhHROYCtS4p/xwF6DdT3bI0+HRU35/xNBTZH2yJN55u9i42Vb0ddCyhzOLZYQ3JVgglty6hLgVeQzuN4b2MoalhfTl11HsElJT1kB0mznVX8CL7UMTUp/2uSgL8DN6kB4owEZ45nWRxIR/2sCidMJ1tnSI1uSqkeqXRf1vat5qPq+BzpYE0Urn2ZZEmgJyb2u0ZLn2x1tfJyPtv+hqBoT7iqJ224dSbuB4HMT49YtcheUtR0jnrImJXm+M1TeIjWB3XWOScrNV8sWEJMAiIU="",""shopperLocale"": ""en-US""}";
+            // Assert that readonly parameters like ID are actually deserialised and can be serialised again.
+            var deserializedResponse = JsonConvert.DeserializeObject<CreateCheckoutSessionResponse>(sessionResponse);
+            Assert.IsNotNull(deserializedResponse.Id);
+            Assert.IsTrue(deserializedResponse.ToJson().Contains("\"id\": \"CS0068299CB8DA273A\","));
         }
 
         private static string GetSaleToPoiMessage(string online)
