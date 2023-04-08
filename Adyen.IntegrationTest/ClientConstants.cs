966ecb4418e52078b44d4efd9a300e5abf25c3e5
jillingk <93914435+jillingk@users.noreply.github.com>
2023-04-08 13:57:19 +0200

    fix

--- Adyen.IntegrationTest/ClientConstants.cs
+++ Adyen.IntegrationTest/ClientConstants.cs
@@ -4,8 +4,8 @@ namespace Adyen.IntegrationTest
 {
     public class ClientConstants
     {
-        public static readonly string MerchantAccount = "TestMerchantAccount";
-        public static readonly string Xapikey = "AQEwhmfxJo3GbBdFw0m/n3Q5qf3VfI5eGbBFVXVXyGH6zDXzQ826g1ALHYQE3/a+9EmJEMFdWw2+5HzctViMSCJMYAc=-sE6uyCVFg+GOhU0dUV1xfseRA28rO0fTby3hoXBN98c=-vV^<3Wp@],Sy,Yr)";
+        public static readonly string MerchantAccount = Environment.GetEnvironmentVariable("INTEGRATION_MERCHANT_ACCOUNT");
+        public static readonly string Xapikey = Environment.GetEnvironmentVariable("INTEGRATION_X_API_KEY");
         
         public static readonly string CaUsername = Environment.GetEnvironmentVariable("INTEGRATION_CA_USERNAME");
         public static readonly string CaPassword = Environment.GetEnvironmentVariable("INTEGRATION_CA_PASSWORD");
