9b67d8db22fb63a67892bc8acc72d805790b5ed6
jillingk <93914435+jillingk@users.noreply.github.com>
2023-04-08 14:48:27 +0200

    add unit tests for baseURL creator method

--- Adyen.Test/ExtensionsTest.cs
+++ Adyen.Test/ExtensionsTest.cs
@@ -1,48 +1,96 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Adyen.Model.BalanceControl;
 using Adyen.Model.Checkout;
+using Adyen.Service;
+using Adyen.Service.BalancePlatform;
+using Adyen.Service.Checkout;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Environment = Adyen.Model.Environment;
 
 namespace Adyen.Test
 {
     [TestClass]
-    public class ExtensionsTest
+    public class ExtensionsTest : BaseTest
     {
+        private Client _client;
+
+        [TestInitialize]
+        public void Init()
+        {
+            _client = CreateMockTestClientApiKeyBasedRequestAsync("");
+            _client.Config.Environment = Environment.Live;
+            _client.Config.LiveEndpointUrlPrefix = "prefix";
+        }
+
         [TestMethod]
         public void TestDeviceRenderOptionsObjectListToString()
         {
             var deviceRenderOptions = new DeviceRenderOptions
             {
                 SdkInterface = DeviceRenderOptions.SdkInterfaceEnum.Native,
-                SdkUiType = new List<DeviceRenderOptions.SdkUiTypeEnum> { DeviceRenderOptions.SdkUiTypeEnum.MultiSelect, DeviceRenderOptions.SdkUiTypeEnum.OtherHtml }
+                SdkUiType = new List<DeviceRenderOptions.SdkUiTypeEnum>
+                    { DeviceRenderOptions.SdkUiTypeEnum.MultiSelect, DeviceRenderOptions.SdkUiTypeEnum.OtherHtml }
             };
             var expected = "\"multiSelect\"";
             Assert.IsTrue(deviceRenderOptions.ToJson().Contains(expected));
         }
 
-        /*[TestMethod]
-        public void TestFraudCheckResultObjectListToString()
-        {
-            //List <FraudREsults> = FraudResults.Result
-            var fraudCheckResult1 = new FraudCheckResultContainer
-            {
-                FraudCheckResult = new FraudCheckResult(AccountScore: 1, Name: "test1", CheckId: 1)
-            };
-            var fraudCheckResult2 = new FraudCheckResultContainer
-            {
-                FraudCheckResult = new FraudCheckResult(AccountScore: 2, Name: "test2", CheckId: 2)
-            };
-            var fraudCheckResultContainers = new List<FraudCheckResultContainer>() { fraudCheckResult1, fraudCheckResult2 };
-            var fraudResult = new FraudResult(AccountScore:25, Results: fraudCheckResultContainers);
-            var expectedString = "class FraudResult {\n  AccountScore: 25\n  Results: \n\t{  class FraudResults {\n  FraudCheckResult: class FraudCheckResult {\n  AccountScore: 1\n  CheckId: 1\n  Name: test1\n}\n\n}\n\n class FraudResults {\n  FraudCheckResult: class FraudCheckResult {\n  AccountScore: 2\n  CheckId: 2\n  Name: test2\n}\n\n}\n\n }\n}\n";
-            Assert.AreEqual(expectedString, fraudResult.ToString());
-        }*/
-
         [TestMethod]
         public void TestToCollectionsStringEmpty()
         {
-            var paymentVerificationResponse = new PaymentVerificationResponse(merchantReference:"ref",shopperLocale:"NL", additionalData:new Dictionary<string, string> {{"scaExemptionRequested","lowValue"}});
+            var paymentVerificationResponse = new PaymentVerificationResponse(merchantReference: "ref",
+                shopperLocale: "NL",
+                additionalData: new Dictionary<string, string> { { "scaExemptionRequested", "lowValue" } });
             var expected = "\"scaExemptionRequested\": \"lowValue\"";
             Assert.IsTrue(paymentVerificationResponse.ToJson().Contains(expected));
+        }
+
+        [TestMethod]
+        public void TestPalLiveEndPoint()
+        {
+            var service = new BalanceControlService(_client);
+            service.BalanceTransfer(new BalanceTransferRequest());
+            ClientInterfaceMock.Verify(mock =>
+                mock.RequestAsync(
+                    "https://prefix-pal-live.adyenpayments.com/pal/servlet/BalanceControl/v1/balanceTransfer",
+                    "{}", null, HttpMethod.Post, default));
+        }
+
+        [TestMethod]
+        public void TestCheckoutLiveEndPoint()
+        {
+            var service = new PaymentsService(_client);
+            service.Donations(new PaymentDonationRequest());
+            ClientInterfaceMock.Verify(mock =>
+                mock.RequestAsync(
+                    "https://prefix-checkout-live.adyenpayments.com/checkout/v70/donations",
+                    "{}", null, HttpMethod.Post, default));
+        }
+
+        [TestMethod]
+        public void TestBclLiveEndPoint()
+        {
+            var service = new AccountHoldersService(_client);
+            service.GetAllBalanceAccountsOfAccountHolder("id", offset: 3, limit: 5);
+            ClientInterfaceMock.Verify(mock =>
+                mock.RequestAsync(
+                    "https://balanceplatform-api-live.adyen.com/bcl/v2/accountHolders/id/balanceAccounts?offset=3&limit=5",
+                    null, null, HttpMethod.Get, default));
+        }
+
+        [TestMethod]
+        public async Task TestPalLiveEndPointNoPrefix()
+        {
+            _client.Config.LiveEndpointUrlPrefix = default;
+            var service = new PaymentsService(_client);
+            try
+            {
+                await service.DonationsAsync(new PaymentDonationRequest());
+                
+            } catch (InvalidOperationException e){ Assert.AreEqual("Missing liveEndpointUrlPrefix for endpoint generation",e.Message);}
         }
     }
 }
