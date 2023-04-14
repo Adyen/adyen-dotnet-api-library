81831935bb1b64a542d531a0ee2dadc4074fcea8
alexandros <alexandros.moraitis@adyen.com>
2023-04-14 13:46:33 +0200

    Rename the NotificationHandler to WebhookHnadler

---
+++ Adyen/Webhooks/WebhookHandler.cs
@@ -0,0 +1,19 @@
+﻿using Adyen.Model.MarketPay.Notification;
+using Adyen.Model.Notification;
+using Adyen.Util;
+
+namespace Adyen.Webhooks
+{
+    public class WebhookHandler
+    {
+        public NotificationRequest HandleNotificationRequest(string jsonRequest)
+        {
+            return JsonOperation.Deserialize<NotificationRequest>(jsonRequest);
+        }
+
+        public IGenericNotification HandleMarketpayNotificationJson(string jsonRequest)
+        {
+            return JsonOperation.Deserialize<IGenericNotification>(jsonRequest);
+        }
+    }
+}
