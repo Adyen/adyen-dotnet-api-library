7a226377596208ad6e17919dd5b44390fd91a4d4
jillingk <93914435+jillingk@users.noreply.github.com>
2023-05-10 15:23:23 +0200

    bump lib versions for v10 full release

--- Adyen/Constants/ClientConfig.cs
+++ Adyen/Constants/ClientConfig.cs
@@ -49,6 +49,6 @@
         public static string DataProtectionVersion = "v1";
 
         public static string LibName = "adyen-dotnet-api-library";
-        public static string LibVersion = "10.0.0-beta";
+        public static string LibVersion = "10.0.0";
     }
 }
