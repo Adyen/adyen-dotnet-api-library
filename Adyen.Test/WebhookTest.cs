668073fc9690e3c19557e24497c12c06c66bfecd
jillingk <93914435+jillingk@users.noreply.github.com>
2023-07-10 18:00:12 +0200

    extra unit test for dynamics

--- Adyen.Test/WebhookTest.cs
+++ Adyen.Test/WebhookTest.cs
@@ -185,7 +185,39 @@ namespace Adyen.Test
             AccountHolderNotificationRequest webhook = handler.GetAccountHolderNotificationRequest();
             Assert.IsNotNull(webhook);
             Assert.AreEqual(webhook.Data.BalancePlatform, "YOUR_BALANCE_PLATFORM");
-
+        }
+        
+        [TestMethod]
+        public void Given_BalancePlatformBalanceAccountHolderCreated_When_Valid_Webhook_Then_Dynamic_Result_Should_Deserialize()
+        {
+            var handler = new BankingWebhookHandler(
+                @"{ 'data': {
+                    'balancePlatform': 'YOUR_BALANCE_PLATFORM',
+                    'accountHolder': {
+                        'contactDetails': {
+                            'address': {
+                                'country': 'NL',
+                                'houseNumberOrName': '274',
+                                'postalCode': '1020CD',
+                                'street': 'Brannan Street'
+                            },
+                            'email': 's.hopper@example.com',
+                            'phone': {
+                                'number': '+315551231234',
+                                'type': 'Mobile'
+                            }
+                        },
+                        'description': 'S.Hopper - Staff 123',
+                        'id': 'AH00000000000000000000001',
+                        'status': 'Active'
+                    }
+                },
+                'environment': 'test',
+                'type': 'balancePlatform.accountHolder.created'
+            }");
+            var webhook = handler.HandleBankingWebhooks();
+            Assert.AreEqual(webhook.GetType(), typeof(AccountHolderNotificationRequest));
+            Assert.AreEqual(webhook.Type, AccountHolderNotificationRequest.TypeEnum.Created);
         }
     }
 }
