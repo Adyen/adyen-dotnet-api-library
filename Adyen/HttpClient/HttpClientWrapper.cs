c33bdd74fefcb427c67de2913d9e8128607c0502
jillingk <93914435+jillingk@users.noreply.github.com>
2023-07-10 20:39:25 +0200

    add support for proxies/certificates to the simple httpclient AdyenClient configuration

--- Adyen/HttpClient/HttpClientWrapper.cs
+++ Adyen/HttpClient/HttpClientWrapper.cs
@@ -1,6 +1,8 @@
 using System;
 using System.Collections.Generic;
 using System.Net.Http;
+using System.Net.Security;
+using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
@@ -8,6 +10,7 @@ using System.Web;
 using Adyen.Constants;
 using Adyen.HttpClient.Interfaces;
 using Adyen.Model;
+using Adyen.Security;
 
 namespace Adyen.HttpClient
 {
@@ -47,43 +50,43 @@ namespace Adyen.HttpClient
         {   
             if (httpMethod == null) {httpMethod = HttpMethod.Post;}
             
-            var httpWebRequest = new HttpRequestMessage(httpMethod, endpoint);
+            var httpRequestMessage = new HttpRequestMessage(httpMethod, endpoint);
             
             // Custom patch method for dotnet <2.1
             var patchMethod = new HttpMethod("PATCH");
             
             if (httpMethod == HttpMethod.Post || httpMethod == patchMethod)
             {
-                httpWebRequest.Content = new StringContent(requestBody, _encoding, "application/json");
+                httpRequestMessage.Content = new StringContent(requestBody, _encoding, "application/json");
             }
 
-            httpWebRequest.Headers.Add("ContentType", "application/json");
-            httpWebRequest.Headers.Add("Accept-Charset", "UTF-8");
-            httpWebRequest.Headers.Add("Cache-Control", "no-cache");
-            httpWebRequest.Headers.Add("UserAgent", $"{_config.ApplicationName} {ClientConfig.UserAgentSuffix}{ClientConfig.LibVersion}");
+            httpRequestMessage.Headers.Add("ContentType", "application/json");
+            httpRequestMessage.Headers.Add("Accept-Charset", "UTF-8");
+            httpRequestMessage.Headers.Add("Cache-Control", "no-cache");
+            httpRequestMessage.Headers.Add("UserAgent", $"{_config.ApplicationName} {ClientConfig.UserAgentSuffix}{ClientConfig.LibVersion}");
             if (!string.IsNullOrWhiteSpace(requestOptions?.IdempotencyKey))
             {
-                httpWebRequest.Headers.Add("Idempotency-Key", requestOptions.IdempotencyKey);
+                httpRequestMessage.Headers.Add("Idempotency-Key", requestOptions.IdempotencyKey);
             }
 
             //Use one of two authentication method.
             if (_config.HasApiKey)
             {
-                httpWebRequest.Headers.Add("x-api-key", _config.XApiKey);
+                httpRequestMessage.Headers.Add("x-api-key", _config.XApiKey);
             }
             else if (_config.HasPassword)
             {
                 var authString = _config.Username + ":" + _config.Password;
                 var bytes = Encoding.UTF8.GetBytes(authString);
                 var credentials = Convert.ToBase64String(bytes);
-                httpWebRequest.Headers.Add("Authorization", "Basic " + credentials);
+                httpRequestMessage.Headers.Add("Authorization", "Basic " + credentials);
             }
             
             // Add library name and version to request for analysis
-            httpWebRequest.Headers.Add(ApiConstants.AdyenLibraryName, ClientConfig.LibName);
-            httpWebRequest.Headers.Add(ApiConstants.AdyenLibraryVersion, ClientConfig.LibVersion);
+            httpRequestMessage.Headers.Add(ApiConstants.AdyenLibraryName, ClientConfig.LibName);
+            httpRequestMessage.Headers.Add(ApiConstants.AdyenLibraryVersion, ClientConfig.LibVersion);
 
-            return httpWebRequest;
+            return httpRequestMessage;
         }
 
         private static string QueryString(IDictionary<string, string> dict)
