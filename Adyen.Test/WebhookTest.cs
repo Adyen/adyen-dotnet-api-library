c5ce7586bfe0fc32bd8be69119720dfa88c94515
jillingk <93914435+jillingk@users.noreply.github.com>
2023-07-10 16:21:37 +0200

    GivenWhenThen test names

--- Adyen.Test/WebhookTest.cs
+++ Adyen.Test/WebhookTest.cs
@@ -144,7 +144,7 @@ namespace Adyen.Test
         }
         
         [TestMethod]
-        public void TestBankingWebhookHmacValidation()
+        public void Given_BalancePlatformBalanceAccountSweep_When_Is_Valid_Hmac_Then_Result_Should_Be_True()
         {
             var notification = "{\"data\":{\"balancePlatform\":\"Integration_tools_test\",\"accountId\":\"BA32272223222H5HVKTBK4MLB\",\"sweep\":{\"id\":\"SWPC42272223222H5HVKV6H8C64DP5\",\"schedule\":{\"type\":\"balance\"},\"status\":\"active\",\"targetAmount\":{\"currency\":\"EUR\",\"value\":0},\"triggerAmount\":{\"currency\":\"EUR\",\"value\":0},\"type\":\"pull\",\"counterparty\":{\"balanceAccountId\":\"BA3227C223222H5HVKT3H9WLC\"},\"currency\":\"EUR\"}},\"environment\":\"test\",\"type\":\"balancePlatform.balanceAccountSweep.updated\"}";
             var signKey = "D7DD5BA6146493707BF0BE7496F6404EC7A63616B7158EC927B9F54BB436765F";
@@ -155,7 +155,7 @@ namespace Adyen.Test
         }
         
         [TestMethod]
-        public void TestBankingWebhooks()
+        public void Given_BalancePlatformBalanceAccountHolderCreated_When_Valid_Webhook_Then_Result_Should_Deserialize()
         {
             var handler = new BankingWebhookHandler();
             AccountHolderNotificationRequest webhook = handler.GetAccountHolderNotificationRequest(@"{
