b35568fea737644afe265913ae79168170af7043
jilling <jilling.kingma@adyen.com>
2023-09-07 17:22:52 +0800

    ssZ format

--- Adyen.Test/TransfersTest.cs
+++ Adyen.Test/TransfersTest.cs
@@ -19,7 +19,7 @@ namespace Adyen.Test
             var service = new TransactionsService(client);
             var response = service.GetAllTransactions(new DateTime(), new DateTime(), balancePlatform:"balance", accountHolderId:"id");
             ClientInterfaceSubstitute.Received().RequestAsync(
-                "https://balanceplatform-api-test.adyen.com/btl/v3/transactions?balancePlatform=balance&accountHolderId=id&createdSince=0001-01-01T00%3a00%3a00%2b06%3a56&createdUntil=0001-01-01T00%3a00%3a00%2b06%3a56",
+                "https://balanceplatform-api-test.adyen.com/btl/v3/transactions?balancePlatform=balance&accountHolderId=id&createdSince=0001-01-01T00%3a00%3a00Z&createdUntil=0001-01-01T00%3a00%3a00Z",
                 null, null, HttpMethod.Get, CancellationToken.None);
             Assert.AreEqual(response.Links.Next.Href, "https://balanceplatform-api-test.adyen.com/btl/v2/transactions?balancePlatform=Bastronaut&createdUntil=2022-03-21T00%3A00%3A00Z&createdSince=2022-03-11T00%3A00%3A00Z&limit=3&cursor=S2B-TSAjOkIrYlIlbjdqe0RreHRyM32lKRSxubXBHRkhHL2E32XitQQz5SfzpucD5HbHwpM1p6NDR1eXVQLFF6MmY33J32sobDxQYT90MHIud1hwLnd6JitcX32xJ");
         }
