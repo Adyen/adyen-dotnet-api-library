15606d4b66615ea4524ca689ba2025b3db97fa90
Aleffio <Aleffio@users.noreply.github.com>
2023-09-11 11:18:19 +0000

    chore(release): bump to 11.1.1

--- Adyen/Constants/ClientConfig.cs
+++ Adyen/Constants/ClientConfig.cs
@@ -14,6 +14,6 @@
         public static string NexoProtocolVersion = "3.0";
 
         public static string LibName = "adyen-dotnet-api-library";
-        public static string LibVersion = "11.1.0";
+        public static string LibVersion = "11.1.1";
     }
 }
