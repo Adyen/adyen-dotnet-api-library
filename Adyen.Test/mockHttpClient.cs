f98bfe807ba28d72df699a559607c338b21a7e1e
Wouter Boereboom <62436079+wboereboom@users.noreply.github.com>
2024-01-16 09:38:06 +0100

    Fix/empty post request (#971)
    * add test for empty post request on httpClientWrapper
    
    * convert test method to not have async in signature
    
    * add post method to http wrapper mock call
    
    * add valid url to httpClientWrapperTest
    
    * use AreEquals instead of Equals
    
    * introduce httpClientWrapper fix

---
+++ Adyen.Test/mockHttpClient.cs
@@ -0,0 +1,34 @@
+using System.Net;
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+
+public class MockHttpMessageHandler : HttpMessageHandler
+{
+    private readonly string _response;
+    private readonly HttpStatusCode _statusCode;
+
+    public string Input { get; private set; }
+    public int NumberOfCalls { get; private set; }
+
+    public MockHttpMessageHandler(string response, HttpStatusCode statusCode)
+    {
+        _response = response;
+        _statusCode = statusCode;
+    }
+
+    protected override async Task<HttpResponseMessage> SendAsync(HttpRequestMessage request,
+        CancellationToken cancellationToken)
+    {
+        NumberOfCalls++;
+        if (request.Content != null) // Could be a GET-request without a body
+        {
+            Input = await request.Content.ReadAsStringAsync();
+        }
+        return new HttpResponseMessage
+        {
+            StatusCode = _statusCode,
+            Content = new StringContent(_response)
+        };
+    }
+}
