9e503ee7f2e348f9ea3f7a1fb8bfca34ca2822ca
Adyen Automation <plugins_dev@adyen.com>
2024-10-09 20:10:01 +0000

    false[adyen-sdk-automation] automated change

--- Adyen/Model/BalancePlatform/AccountHolder.cs
+++ Adyen/Model/BalancePlatform/AccountHolder.cs
@@ -116,7 +116,7 @@ namespace Adyen.Model.BalancePlatform
         /// Gets or Sets ContactDetails
         /// </summary>
         [DataMember(Name = "contactDetails", EmitDefaultValue = false)]
-        [Obsolete]
+        [Obsolete("Deprecated since Configuration API v - ")]
         public ContactDetails ContactDetails { get; set; }
 
         /// <summary>
