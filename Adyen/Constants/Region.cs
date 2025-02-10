23dba27750bb429d075ae153c1f469146475d345
Djoyke Reijans <115019123+DjoykeAbyah@users.noreply.github.com>
2025-02-10 11:57:31 +0100

    Add regional endpoint functionalities (#1108)
    * Added endpoint mapping per region
    
    Added RegionTest.cs
    
    Fix: Update tests so that test returns testUrls and live returns liveUrls for terminalapi
    
    * Add comments
    
    ---------
    
    Co-authored-by: kwokhe <kwokhe.chu@adyen.com>

--- Adyen/Constants/Region.cs
+++ Adyen/Constants/Region.cs
@@ -1,21 +1,53 @@
+using System.Collections.Generic;
+
 namespace Adyen.Constants
 {
     /// <summary>
-    /// Enum to set region based urls for terminal api
-    /// https://docs.adyen.com/point-of-sale/design-your-integration/terminal-api/
+    /// Enum to set region-based URLs for the Terminal API.
+    /// For all possible values refer to: https://docs.adyen.com/point-of-sale/design-your-integration/terminal-api/#live-endpoints
     /// </summary>
     public enum Region
     {
-        //Europe
+        /// <summary>
+        /// European Union region.
+        /// </summary>
         EU = 0,
 
-        //Australia
+        /// <summary>
+        /// Australia region.
+        /// </summary>
         AU = 1,
 
-        //US
+        /// <summary>
+        /// United States region.
+        /// </summary>
         US = 2,
 
-        //East Asia
-        APSE = 3
+        /// <summary>
+        /// Asia-Pacific, South East region.
+        /// </summary>
+        APSE = 3,
+
+        /// <summary>
+        /// India region.
+        /// </summary>
+        IN = 4
+    }
+
+    /// <summary>
+    /// A static class that provides mapping of regions to their respective Terminal API endpoints.
+    /// </summary>
+    public static class RegionMapping
+    {
+        /// <summary>
+        /// Maps regions to their respective Terminal API live endpoints.
+        /// </summary>
+        public static readonly Dictionary<Region, string> TERMINAL_API_ENDPOINTS_MAPPING = new Dictionary<Region, string>()
+        {
+            { Region.EU, "https://terminal-api-live.adyen.com" },
+            { Region.AU, "https://terminal-api-live-au.adyen.com" },
+            { Region.US, "https://terminal-api-live-us.adyen.com" },
+            { Region.APSE, "https://terminal-api-live-apse.adyen.com" }
+        };
     }
 }
