d7b331c2a397c762c6fd2ffc0e11864ba1131baf
Pete W <peterwishart@users.noreply.github.com>
2025-03-17 13:34:23 +0000

    Clean up a bit

--- Adyen.Test/DeserializerTest.cs
+++ Adyen.Test/DeserializerTest.cs
@@ -10,19 +10,31 @@ namespace Adyen.Test
         [TestMethod]
         public void PrinterStatusSerializerTest()
         {
-            string GetDiagnosisRequest(string poiStatusJson)
-            {
-                var diagnosisTemplate = "{\"SaleToPOIMessage\":{\"MessageHeader\":{\"MessageClass\":\"Service\",\"MessageCategory\":\"Diagnosis\",\"MessageType\":\"Response\",\"ServiceID\":\"12345678\",\"SaleID\":\"POSSystemID12345\",\"POIID\":\"V400m-284251016\",\"ProtocolVersion\":\"3.0\"},\"DiagnosisResponse\":{\"Response\":{\"Result\":\"Success\"},POI_STATUS,\"HostStatus\":[{\"IsReachableFlag\":true}]}}}";
-                return diagnosisTemplate.Replace("POI_STATUS", poiStatusJson);
-            }
-
             var saleToPoiMessageSerializer = new SaleToPoiMessageSerializer();
+            string GetDiagnosisResponse(string poiStatusJson)
+            {
+                var diagnosisResponseMessage = new SaleToPOIMessage()
+                {
+                    MessageHeader = new MessageHeader() { 
+                        MessageClass = MessageClassType.Service, 
+                        MessageCategory = MessageCategoryType.Diagnosis, 
+                        MessageType = MessageType.Response, 
+                        ServiceID = "12345678", 
+                        SaleID = "POSSystemID12345", 
+                        POIID = "V400m-284251016" 
+                    },
+                    MessagePayload = new DiagnosisResponse() 
+                };
+                var diagnosisTemplate = saleToPoiMessageSerializer.Serialize(diagnosisResponseMessage);
+                var diagnosisStart = @"""DiagnosisResponse"":{";
+                var insertIndex = diagnosisTemplate.IndexOf(diagnosisStart) + diagnosisStart.Length;
+                return diagnosisTemplate.Insert(insertIndex, poiStatusJson);
+            }
+            var poiStatusPrinterOk = @"""POIStatus"":{""CommunicationOKFlag"":true,""GlobalStatus"":""OK"",""PrinterStatus"":""OK""}";
+            var poiStatusPrinterAbsent = @"""POIStatus"":{""CommunicationOKFlag"":true,""GlobalStatus"":""OK""}";
 
-            var poiStatusPrinterOk = "\"POIStatus\":{\"CommunicationOKFlag\":true,\"GlobalStatus\":\"OK\",\"PrinterStatus\":\"OK\"}";
-            var poiStatusPrinterAbsent = "\"POIStatus\":{\"CommunicationOKFlag\":true,\"GlobalStatus\":\"OK\"}";
-
-            var parsedPrinterOk = saleToPoiMessageSerializer.Deserialize(GetDiagnosisRequest(poiStatusPrinterOk));
-            var parsedPrinterAbsent = saleToPoiMessageSerializer.Deserialize(GetDiagnosisRequest(poiStatusPrinterAbsent));
+            var parsedPrinterOk = saleToPoiMessageSerializer.Deserialize(GetDiagnosisResponse(poiStatusPrinterOk));
+            var parsedPrinterAbsent = saleToPoiMessageSerializer.Deserialize(GetDiagnosisResponse(poiStatusPrinterAbsent));
 
             Assert.IsNotNull((parsedPrinterOk.MessagePayload as DiagnosisResponse).POIStatus.PrinterStatus);
             Assert.AreEqual(PrinterStatusType.OK, (parsedPrinterOk.MessagePayload as DiagnosisResponse).POIStatus.PrinterStatus);
