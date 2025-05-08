865d70cba357e6a7230328db6f833a4159517d49
kwokhe <kwokhe.chu@adyen.com>
2025-05-08 15:20:04 +0200

    Upgrade to .NET8.0
    Support net462

--- Adyen/Config.cs
+++ Adyen/Config.cs
@@ -6,22 +6,32 @@ namespace Adyen
 {
     public class Config
     {
-        //Merchant details
         public string Username { get; set; }
         public string Password { get; set; }
+        public bool HasPassword => !string.IsNullOrEmpty(Password);
+        
         public Environment Environment { get; set; }
         public string LiveEndpointUrlPrefix { get; set; }
         public string ApplicationName { get; set; }
         public IWebProxy Proxy { get; set; }
-        //Terminal cloud api
+        
+        /// <summary>
+        /// Your Adyen API key.
+        /// </summary>
         public string XApiKey { get; set; }
-        // Timeout in milliseconds
+        public bool HasApiKey => !string.IsNullOrEmpty(XApiKey);
+        
+        /// <summary>
+        /// Timeout in milliseconds.
+        /// </summary>
         public int Timeout { get; set; }
         public string CloudApiEndPoint { get; set; }
-        // Local TAPI endpoint
+        
+        /// <summary>
+        /// The url of the Terminal Api endpoint, can be overriden if you want to send local terminal-api requests.
+        /// </summary>
         public string LocalTerminalApiEndpoint { get; set; }
-        public bool HasPassword => !string.IsNullOrEmpty(Password);
-        public bool HasApiKey => !string.IsNullOrEmpty(XApiKey);
+        
         public BaseUrlConfig BaseUrlConfig { get; set; }
         
         public Region TerminalApiRegion { get; set; } 
