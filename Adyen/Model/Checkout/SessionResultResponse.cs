4b48f087f56b988bda73abd8d1a027234c5071c1
beppe <beppe.catanese@adyen.com>
2025-07-14 09:54:29 +0200

    Retain and deprecate previous constructor

--- Adyen/Model/Checkout/SessionResultResponse.cs
+++ Adyen/Model/Checkout/SessionResultResponse.cs
@@ -87,6 +87,18 @@ namespace Adyen.Model.Checkout
         /// <summary>
         /// Initializes a new instance of the <see cref="SessionResultResponse" /> class.
         /// </summary>
+        /// <param name="id">A unique identifier of the session..</param>
+        /// <param name="status">The status of the session. The status included in the response doesn&#39;t get updated. Don&#39;t make the request again to check for payment status updates.  Possible values: * **completed**: the shopper completed the payment, and the payment was authorized. * **paymentPending**: the shopper is in the process of making the payment. This applies to payment methods with an asynchronous flow, like voucher payments where the shopper completes the payment in a physical shop. * **refused**: the session has been refused, because of too many refused payment attempts. The shopper can no longer complete the payment with this session. * **canceled**: the shopper canceled the payment. * **expired**: the session expired. The shopper can no longer complete the payment with this session. By default, the session expires one hour after it is created..</param>
+        [Obsolete("Use the constructor with additionalData, payments, reference, and status instead.")]
+        public SessionResultResponse(string id = default(string), StatusEnum? status = default(StatusEnum?))
+        {
+            this.Id = id;
+            this.Status = status;
+        }        
+        
+        /// <summary>
+        /// Initializes a new instance of the <see cref="SessionResultResponse" /> class.
+        /// </summary>
         /// <param name="additionalData">Contains additional information about the payment. Some fields are included only if you enable them. To enable these fields in your Customer Area, go to **Developers** &gt; **Additional data**..</param>
         /// <param name="id">A unique identifier of the session..</param>
         /// <param name="payments">A list of all authorised payments done for this session..</param>
