7fb887d076b4f33bffa5eafecebbd4e4a556c057
beppe <beppe.catanese@adyen.com>
2025-11-12 10:41:57 +0100

    Refactor LEM test

---
+++ Adyen.Test/LegalEntityManagement/LegalEntities/LegalEntityTest.cs
@@ -0,0 +1,101 @@
+using System.Net;
+using System.Text.Json;
+using Adyen.Core;
+using Adyen.Core.Options;
+using Adyen.LegalEntityManagement.Client;
+using Adyen.LegalEntityManagement.Extensions;
+using Adyen.LegalEntityManagement.Models;
+using Adyen.LegalEntityManagement.Services;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NSubstitute;
+
+namespace Adyen.Test.LegalEntityManagement.LegalEntities
+{
+    [TestClass]
+    public class LegalEntityTest
+    {
+        private readonly JsonSerializerOptionsProvider _jsonSerializerOptionsProvider;
+        private readonly ILegalEntitiesService _legalEntitiesService;
+        
+        public LegalEntityTest()
+        {
+            IHost testHost = Host.CreateDefaultBuilder()
+                .ConfigureLegalEntityManagement((context, services, config) =>
+                {
+                    config.ConfigureAdyenOptions(options =>
+                    {
+                        options.Environment = AdyenEnvironment.Test;
+                    });
+                })
+                .Build();
+            _jsonSerializerOptionsProvider = testHost.Services.GetRequiredService<JsonSerializerOptionsProvider>();
+            _legalEntitiesService = Substitute.For<ILegalEntitiesService>();
+
+        }
+
+        /// <summary>
+        /// Test DeserializeLegalEntity
+        /// </summary>
+        [TestMethod]
+        public void DeserializeLegalEntity()
+        {
+            // Arrange
+            var json = TestUtilities.GetTestFileContent("mocks/legalentitymanagement/LegalEntity.json");
+
+            // Act
+            var result = JsonSerializer.Deserialize<LegalEntity>(json, _jsonSerializerOptionsProvider.Options);
+
+            // Assert
+            Assert.IsNotNull(result);
+            Assert.AreEqual("LE322JV223222D5GG42KN6869", result.Id);
+        }
+        
+        /// <summary>
+        /// Test DeserializeLegalEntityBusinessLines
+        /// </summary>
+        [TestMethod]
+        public void DeserializeLegalEntityBusinessLines()
+        {
+            // Arrange
+            var json = TestUtilities.GetTestFileContent("mocks/legalentitymanagement/LegalEntityBusinessLines.json");
+
+            // Act
+            var result = JsonSerializer.Deserialize<Adyen.LegalEntityManagement.Models.BusinessLines>(json, _jsonSerializerOptionsProvider.Options);
+
+            // Assert
+            Assert.IsNotNull(result);
+            Assert.AreEqual(2, result.VarBusinessLines.Count);
+        }
+        
+        /// <summary>
+        /// Test GetLegalEntity
+        /// </summary>
+        [TestMethod]
+        public void GetLegalEntity()
+        {
+            var json = TestUtilities.GetTestFileContent("mocks/legalentitymanagement/LegalEntity.json");
+            
+            _legalEntitiesService.GetLegalEntityAsync(Arg.Any<string>())
+                .Returns(
+                    Task.FromResult<IGetLegalEntityApiResponse>(
+                        new LegalEntitiesService.GetLegalEntityApiResponse(
+                            Substitute.For<Microsoft.Extensions.Logging.ILogger<LegalEntitiesService.GetLegalEntityApiResponse>>(),
+                            new HttpRequestMessage(), 
+                            new HttpResponseMessage(), 
+                            json, 
+                            "/documents/DOC01234", 
+                            DateTime.UtcNow,
+                            _jsonSerializerOptionsProvider.Options)
+                    ));
+            
+            IGetLegalEntityApiResponse response = _legalEntitiesService.GetLegalEntityAsync("LE322JV223222D5GG42KN6869", CancellationToken.None).Result;
+            
+            Assert.IsNotNull(response);
+            Assert.AreEqual(HttpStatusCode.OK, response.StatusCode);
+            Assert.AreEqual("LE322JV223222D5GG42KN6869", response.Ok().Id);
+        }
+        
+    }
+}
