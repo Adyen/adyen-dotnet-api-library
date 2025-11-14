ac8c36a98d6fa27855b8e81e70833c7076c8d782
beppe <beppe.catanese@adyen.com>
2025-11-14 14:37:45 +0100

    Add TestClass annotation and new test

--- Adyen.Test/Core/Utilities/HmacValidatorUtilityTest.cs
+++ Adyen.Test/Core/Utilities/HmacValidatorUtilityTest.cs
@@ -3,6 +3,7 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace Adyen.Test.Core.Utilities
 {
+    [TestClass]
     public class HmacValidatorUtilityTest
     {
         [TestMethod]
@@ -19,6 +20,22 @@ namespace Adyen.Test.Core.Utilities
             
             // Assert
             Assert.IsTrue(isValidSignature);
+        }
+
+        [TestMethod]
+        public void GenerateBase64Sha256HmacSignature_ReturnsCorrectSignature()
+        {
+            // Arrange
+            string notification =
+                "{\"data\":{\"balancePlatform\":\"Integration_tools_test\",\"accountId\":\"BA32272223222H5HVKTBK4MLB\",\"sweep\":{\"id\":\"SWPC42272223222H5HVKV6H8C64DP5\",\"schedule\":{\"type\":\"balance\"},\"status\":\"active\",\"targetAmount\":{\"currency\":\"EUR\",\"value\":0},\"triggerAmount\":{\"currency\":\"EUR\",\"value\":0},\"type\":\"pull\",\"counterparty\":{\"balanceAccountId\":\"BA3227C223222H5HVKT3H9WLC\"},\"currency\":\"EUR\"}},\"environment\":\"test\",\"type\":\"balancePlatform.balanceAccountSweep.updated\"}";
+            string hmacKey = "D7DD5BA6146493707BF0BE7496F6404EC7A63616B7158EC927B9F54BB436765F";
+            string expectedHmacSignature = "9Qz9S/0xpar1klkniKdshxpAhRKbiSAewPpWoxKefQA=";
+
+            // Act
+            string generatedHmacSignature = HmacValidatorUtility.GenerateBase64Sha256HmacSignature(notification, hmacKey);
+
+            // Assert
+            Assert.AreEqual(expectedHmacSignature, generatedHmacSignature);
         }
     }
 }
