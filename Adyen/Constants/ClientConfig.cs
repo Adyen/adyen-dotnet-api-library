2a0fd94342d6d1addda5e8f86a9684c95aa1337a
gcatanese <1771700+gcatanese@users.noreply.github.com>
2026-01-12 12:06:55 +0000

    chore(release): bump to 33.0.0

--- Adyen/Constants/ClientConfig.cs
+++ Adyen/Constants/ClientConfig.cs
@@ -15,6 +15,6 @@
         public const string NexoProtocolVersion = "3.0";
 
         public const string LibName = "adyen-dotnet-api-library";
-        public const string LibVersion = "32.2.1";
+        public const string LibVersion = "33.0.0";
     }
 }
