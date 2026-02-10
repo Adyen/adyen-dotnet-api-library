414d44b3ace14ec8acec0d94a0c95aa70eaaab20
AdyenAutomationBot <38424300+AdyenAutomationBot@users.noreply.github.com>
2026-02-10 18:13:02 +0000

    [dataprotection] Automated update from Adyen/adyen-openapi@9c9bc16

--- Adyen/DataProtection/Models/SubjectErasureByPspReferenceRequest.cs
+++ Adyen/DataProtection/Models/SubjectErasureByPspReferenceRequest.cs
@@ -204,7 +204,7 @@ namespace Adyen.DataProtection.Models
         /// <summary>
         /// Serializes the properties of <see cref="SubjectErasureByPspReferenceRequest"/>.
         /// </summary>
-        /// <param name="writer"><see creft="Utf8JsonWriter"/></param>
+        /// <param name="writer"><see cref="Utf8JsonWriter"/></param>
         /// <param name="subjectErasureByPspReferenceRequest"></param>
         /// <param name="jsonSerializerOptions"><see cref="JsonSerializerOptions"/></param>
         /// <exception cref="NotImplementedException"></exception>
--- Adyen/DataProtection/Models/SubjectErasureResponse.cs
+++ Adyen/DataProtection/Models/SubjectErasureResponse.cs
@@ -284,7 +284,7 @@ namespace Adyen.DataProtection.Models
         /// <summary>
         /// Serializes the properties of <see cref="SubjectErasureResponse"/>.
         /// </summary>
-        /// <param name="writer"><see creft="Utf8JsonWriter"/></param>
+        /// <param name="writer"><see cref="Utf8JsonWriter"/></param>
         /// <param name="subjectErasureResponse"></param>
         /// <param name="jsonSerializerOptions"><see cref="JsonSerializerOptions"/></param>
         /// <exception cref="NotImplementedException"></exception>
