63f7c8eb4392361facf4d4960746fa94c355267c
AdyenAutomationBot <38424300+AdyenAutomationBot@users.noreply.github.com>
2026-02-10 18:13:01 +0000

    [sessionauthentication] Automated update from Adyen/adyen-openapi@9c9bc16

--- Adyen/SessionAuthentication/Models/BalanceAccountResource.cs
+++ Adyen/SessionAuthentication/Models/BalanceAccountResource.cs
@@ -153,7 +153,7 @@ namespace Adyen.SessionAuthentication.Models
         /// <summary>
         /// Serializes the properties of <see cref="BalanceAccountResource"/>.
         /// </summary>
-        /// <param name="writer"><see creft="Utf8JsonWriter"/></param>
+        /// <param name="writer"><see cref="Utf8JsonWriter"/></param>
         /// <param name="balanceAccountResource"></param>
         /// <param name="jsonSerializerOptions"><see cref="JsonSerializerOptions"/></param>
         /// <exception cref="NotImplementedException"></exception>
