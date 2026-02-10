1ae46de6dbc17deec407be77da8817e72b2d8be4
AdyenAutomationBot <38424300+AdyenAutomationBot@users.noreply.github.com>
2026-02-10 18:12:52 +0000

    [tokenizationwebhooks] Automated update from Adyen/adyen-openapi@9c9bc16

--- Adyen/TokenizationWebhooks/Models/TokenizationDisabledDetailsNotificationRequest.cs
+++ Adyen/TokenizationWebhooks/Models/TokenizationDisabledDetailsNotificationRequest.cs
@@ -452,7 +452,7 @@ namespace Adyen.TokenizationWebhooks.Models
         /// <summary>
         /// Serializes the properties of <see cref="TokenizationDisabledDetailsNotificationRequest"/>.
         /// </summary>
-        /// <param name="writer"><see creft="Utf8JsonWriter"/></param>
+        /// <param name="writer"><see cref="Utf8JsonWriter"/></param>
         /// <param name="tokenizationDisabledDetailsNotificationRequest"></param>
         /// <param name="jsonSerializerOptions"><see cref="JsonSerializerOptions"/></param>
         /// <exception cref="NotImplementedException"></exception>
--- Adyen/TokenizationWebhooks/Models/TokenizationNotificationResponse.cs
+++ Adyen/TokenizationWebhooks/Models/TokenizationNotificationResponse.cs
@@ -162,7 +162,7 @@ namespace Adyen.TokenizationWebhooks.Models
         /// <summary>
         /// Serializes the properties of <see cref="TokenizationNotificationResponse"/>.
         /// </summary>
-        /// <param name="writer"><see creft="Utf8JsonWriter"/></param>
+        /// <param name="writer"><see cref="Utf8JsonWriter"/></param>
         /// <param name="tokenizationNotificationResponse"></param>
         /// <param name="jsonSerializerOptions"><see cref="JsonSerializerOptions"/></param>
         /// <exception cref="NotImplementedException"></exception>
