df19ea559d3f53c4725835f4fc7dd16d5a592dfa
AdyenAutomationBot <38424300+AdyenAutomationBot@users.noreply.github.com>
2026-02-10 18:13:03 +0000

    [balancecontrol] Automated update from Adyen/adyen-openapi@9c9bc16

--- Adyen/BalanceControl/Models/BalanceTransferResponse.cs
+++ Adyen/BalanceControl/Models/BalanceTransferResponse.cs
@@ -579,7 +579,7 @@ namespace Adyen.BalanceControl.Models
         /// <summary>
         /// Serializes the properties of <see cref="BalanceTransferResponse"/>.
         /// </summary>
-        /// <param name="writer"><see creft="Utf8JsonWriter"/></param>
+        /// <param name="writer"><see cref="Utf8JsonWriter"/></param>
         /// <param name="balanceTransferResponse"></param>
         /// <param name="jsonSerializerOptions"><see cref="JsonSerializerOptions"/></param>
         /// <exception cref="NotImplementedException"></exception>
