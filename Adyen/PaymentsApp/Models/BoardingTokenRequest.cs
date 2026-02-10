de59a29bf560597fe1a33056ba921608dbd45e97
AdyenAutomationBot <38424300+AdyenAutomationBot@users.noreply.github.com>
2026-02-10 18:12:58 +0000

    [paymentsapp] Automated update from Adyen/adyen-openapi@9c9bc16

--- Adyen/PaymentsApp/Models/BoardingTokenRequest.cs
+++ Adyen/PaymentsApp/Models/BoardingTokenRequest.cs
@@ -157,7 +157,7 @@ namespace Adyen.PaymentsApp.Models
         /// <summary>
         /// Serializes the properties of <see cref="BoardingTokenRequest"/>.
         /// </summary>
-        /// <param name="writer"><see creft="Utf8JsonWriter"/></param>
+        /// <param name="writer"><see cref="Utf8JsonWriter"/></param>
         /// <param name="boardingTokenRequest"></param>
         /// <param name="jsonSerializerOptions"><see cref="JsonSerializerOptions"/></param>
         /// <exception cref="NotImplementedException"></exception>
--- Adyen/PaymentsApp/Models/PaymentsAppDto.cs
+++ Adyen/PaymentsApp/Models/PaymentsAppDto.cs
@@ -212,7 +212,7 @@ namespace Adyen.PaymentsApp.Models
         /// <summary>
         /// Serializes the properties of <see cref="PaymentsAppDto"/>.
         /// </summary>
-        /// <param name="writer"><see creft="Utf8JsonWriter"/></param>
+        /// <param name="writer"><see cref="Utf8JsonWriter"/></param>
         /// <param name="paymentsAppDto"></param>
         /// <param name="jsonSerializerOptions"><see cref="JsonSerializerOptions"/></param>
         /// <exception cref="NotImplementedException"></exception>
