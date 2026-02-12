52b643a2e0b1347ac508da7ef955f79621039806
AdyenAutomationBot <38424300+AdyenAutomationBot@users.noreply.github.com>
2026-02-12 19:21:30 +0000

    [paymentsapp] Automated update from Adyen/adyen-openapi@9c9bc16

--- Adyen/PaymentsApp/Extensions/ServiceCollectionExtensions.cs
+++ Adyen/PaymentsApp/Extensions/ServiceCollectionExtensions.cs
@@ -13,7 +13,10 @@ using System;
 using System.Linq;
 using Microsoft.Extensions.DependencyInjection;
 using Adyen.Core.Auth;
+using Adyen.Core.Client;
 using Adyen.PaymentsApp.Client;
+using Adyen.PaymentsApp.Services;
+
 
 namespace Adyen.PaymentsApp.Extensions
 {
@@ -22,18 +25,46 @@ namespace Adyen.PaymentsApp.Extensions
     /// </summary>
     public static class ServiceCollectionExtensions
     {
+
         /// <summary>
-        /// Add the Adyen PaymentsApp API services to your <see cref="IServiceCollection"/>.
+        /// Add all PaymentsApp services using the extension methods in <see cref="ServiceCollectionExtensions"/> and configures the <see cref="System.Net.Http.HttpClient"/> and <see cref="IHttpClientBuilder"/>.
+        /// See: <see cref="PaymentsAppService"/>, 
         /// </summary>
         /// <param name="services"><see cref="IServiceCollection"/>.</param>
-        /// <param name="hostConfigurationOptions">Configures the <see cref="HostConfiguration"/>.</param>
+        /// <param name="serviceLifetime"><see cref="ServiceLifetime"/>.</param>
         /// <param name="httpClientOptions">Configures the <see cref="System.Net.Http.HttpClient"/>.</param>
         /// <param name="httpClientBuilderOptions">Configures the <see cref="IHttpClientBuilder"/>.</param>
-        public static void AddPaymentsAppServices(this IServiceCollection services, Action<HostConfiguration> hostConfigurationOptions, Action<System.Net.Http.HttpClient>? httpClientOptions = null, Action<IHttpClientBuilder>? httpClientBuilderOptions = null)
-        {
-            HostConfiguration hostConfiguration = new(services);
-            hostConfigurationOptions(hostConfiguration);
-            hostConfiguration.AddPaymentsAppHttpClients(httpClientOptions, httpClientBuilderOptions);
+        /// <returns><see cref="IServiceCollection"/>.</returns>
+        public static IServiceCollection AddAllPaymentsAppServices(this IServiceCollection services, ServiceLifetime serviceLifetime = ServiceLifetime.Singleton, Action<System.Net.Http.HttpClient>? httpClientOptions = null, Action<IHttpClientBuilder>? httpClientBuilderOptions = null)
+        {            
+            services.AddPaymentsAppService(serviceLifetime, httpClientOptions, httpClientBuilderOptions);
+            
+            return services;
         }
+
+
+
+        /// <summary>
+        /// Add <see cref="PaymentsAppService"/> as the implementation of <see cref="IPaymentsAppService"/> to the Dependency Injection container <see cref="IServiceCollection"/>. 
+        /// </summary>
+        /// <param name="services"><see cref="IServiceCollection"/>.</param>
+        /// <param name="serviceLifetime">Configures the <see cref="ServiceLifetime"/>, defaults to <see cref="ServiceLifetime.Singleton"/>.</param>
+        /// <returns><see cref="IServiceCollection"/>.</returns>
+        public static IServiceCollection AddPaymentsAppService(this IServiceCollection services, ServiceLifetime serviceLifetime = ServiceLifetime.Singleton, Action<System.Net.Http.HttpClient>? httpClientOptions = null, Action<IHttpClientBuilder>? httpClientBuilderOptions = null)
+        {
+            services.AddSingleton<IApiFactory, ApiFactory>();
+            services.AddSingleton<PaymentsAppServiceEvents>();
+
+            services.Add(new ServiceDescriptor(typeof(IPaymentsAppService), typeof(PaymentsAppService), serviceLifetime));
+
+            IHttpClientBuilder builder = services.AddHttpClient<IPaymentsAppService, PaymentsAppService>(httpClient =>
+            {
+                httpClientOptions?.Invoke(httpClient);
+            });
+
+            httpClientBuilderOptions?.Invoke(builder);
+            return services;
+        }
+
     }
 }
--- Adyen/PaymentsApp/Models/PaymentsAppResponse.cs
+++ Adyen/PaymentsApp/Models/PaymentsAppResponse.cs
@@ -29,7 +29,7 @@ namespace Adyen.PaymentsApp.Models
     /// <summary>
     /// PaymentsAppResponse.
     /// </summary>
-    public partial class PaymentsAppResponse : IValidatableObject
+    public partial class PaymentsAppResponse
     {
         /// <summary>
         /// Initializes a new instance of the <see cref="PaymentsAppResponse" /> class.
@@ -69,16 +69,6 @@ namespace Adyen.PaymentsApp.Models
             sb.Append("  PaymentsApps: ").Append(PaymentsApps).Append("\n");
             sb.Append("}\n");
             return sb.ToString();
-        }
-
-        /// <summary>
-        /// To validate all properties of the instance
-        /// </summary>
-        /// <param name="validationContext">Validation context</param>
-        /// <returns>Validation Result</returns>
-        IEnumerable<ValidationResult> IValidatableObject.Validate(ValidationContext validationContext)
-        {
-            yield break;
         }
     }
 
