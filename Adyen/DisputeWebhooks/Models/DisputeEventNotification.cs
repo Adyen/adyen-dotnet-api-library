d04f272d31392f65ba25bf084f7510ee574dec5b
AdyenAutomationBot <38424300+AdyenAutomationBot@users.noreply.github.com>
2026-02-12 19:21:44 +0000

    [disputewebhooks] Automated update from Adyen/adyen-openapi@9c9bc16

--- Adyen/DisputeWebhooks/Models/DisputeEventNotification.cs
+++ Adyen/DisputeWebhooks/Models/DisputeEventNotification.cs
@@ -29,7 +29,7 @@ namespace Adyen.DisputeWebhooks.Models
     /// <summary>
     /// DisputeEventNotification.
     /// </summary>
-    public partial class DisputeEventNotification : IValidatableObject
+    public partial class DisputeEventNotification
     {
         /// <summary>
         /// Initializes a new instance of the <see cref="DisputeEventNotification" /> class.
@@ -349,16 +349,6 @@ namespace Adyen.DisputeWebhooks.Models
             sb.Append("  Type: ").Append(Type).Append("\n");
             sb.Append("}\n");
             return sb.ToString();
-        }
-
-        /// <summary>
-        /// To validate all properties of the instance
-        /// </summary>
-        /// <param name="validationContext">Validation context</param>
-        /// <returns>Validation Result</returns>
-        IEnumerable<ValidationResult> IValidatableObject.Validate(ValidationContext validationContext)
-        {
-            yield break;
         }
     }
 
--- Adyen/DisputeWebhooks/Models/DisputeNotificationRequest.cs
+++ Adyen/DisputeWebhooks/Models/DisputeNotificationRequest.cs
@@ -29,7 +29,7 @@ namespace Adyen.DisputeWebhooks.Models
     /// <summary>
     /// DisputeNotificationRequest.
     /// </summary>
-    public partial class DisputeNotificationRequest : IValidatableObject
+    public partial class DisputeNotificationRequest
     {
         /// <summary>
         /// Initializes a new instance of the <see cref="DisputeNotificationRequest" /> class.
@@ -181,16 +181,6 @@ namespace Adyen.DisputeWebhooks.Models
             sb.Append("  Type: ").Append(Type).Append("\n");
             sb.Append("}\n");
             return sb.ToString();
-        }
-
-        /// <summary>
-        /// To validate all properties of the instance
-        /// </summary>
-        /// <param name="validationContext">Validation context</param>
-        /// <returns>Validation Result</returns>
-        IEnumerable<ValidationResult> IValidatableObject.Validate(ValidationContext validationContext)
-        {
-            yield break;
         }
     }
 
