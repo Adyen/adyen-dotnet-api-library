3ddba13bae710ea300bd8ab9becac6f81f2d0f5f
AdyenAutomationBot <38424300+AdyenAutomationBot@users.noreply.github.com>
2026-02-12 19:21:38 +0000

    [balancecontrol] Automated update from Adyen/adyen-openapi@9c9bc16

--- Adyen/BalanceControl/Models/BalanceTransferRequest.cs
+++ Adyen/BalanceControl/Models/BalanceTransferRequest.cs
@@ -29,7 +29,7 @@ namespace Adyen.BalanceControl.Models
     /// <summary>
     /// BalanceTransferRequest.
     /// </summary>
-    public partial class BalanceTransferRequest : IValidatableObject
+    public partial class BalanceTransferRequest
     {
         /// <summary>
         /// Initializes a new instance of the <see cref="BalanceTransferRequest" /> class.
@@ -271,28 +271,6 @@ namespace Adyen.BalanceControl.Models
             sb.Append("  Reference: ").Append(Reference).Append("\n");
             sb.Append("}\n");
             return sb.ToString();
-        }
-
-        /// <summary>
-        /// To validate all properties of the instance
-        /// </summary>
-        /// <param name="validationContext">Validation context</param>
-        /// <returns>Validation Result</returns>
-        IEnumerable<ValidationResult> IValidatableObject.Validate(ValidationContext validationContext)
-        {
-            // Description (string) maxLength
-            if (this.Description != null && this.Description.Length > 140)
-            {
-                yield return new ValidationResult("Invalid value for Description, length must be less than 140.", new [] { "Description" });
-            }
-
-            // Reference (string) maxLength
-            if (this.Reference != null && this.Reference.Length > 80)
-            {
-                yield return new ValidationResult("Invalid value for Reference, length must be less than 80.", new [] { "Reference" });
-            }
-
-            yield break;
         }
     }
 
