9d5aed762cb1984b84ee53da2ddda0fcb95d8f14
AdyenAutomationBot <38424300+AdyenAutomationBot@users.noreply.github.com>
2026-05-29 13:48:59 +0000

    [managementwebhooks] Automated update from Adyen/adyen-openapi@1e3919b

--- Adyen/ManagementWebhooks/Models/TerminalSettingsData.cs
+++ Adyen/ManagementWebhooks/Models/TerminalSettingsData.cs
@@ -25,6 +25,7 @@ using System.Text.Json;
 using System.Text.Json.Serialization;
 using System.ComponentModel.DataAnnotations;
 using Adyen.Core;
+using Adyen.Core.Converters;
 using Adyen.ManagementWebhooks.Client;
 
 namespace Adyen.ManagementWebhooks.Models
@@ -32,6 +33,7 @@ namespace Adyen.ManagementWebhooks.Models
     /// <summary>
     /// TerminalSettingsData.
     /// </summary>
+    [JsonConverter(typeof(TerminalSettingsDataJsonConverter))]
     public partial class TerminalSettingsData
     {
         /// <summary>
