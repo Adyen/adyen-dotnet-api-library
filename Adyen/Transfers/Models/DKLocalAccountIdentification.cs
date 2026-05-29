0097fcaaf2c9971b2d76ed87608d657b387debb5
AdyenAutomationBot <38424300+AdyenAutomationBot@users.noreply.github.com>
2026-05-29 13:49:02 +0000

    [transfers] Automated update from Adyen/adyen-openapi@1e3919b

--- Adyen/Transfers/Models/DKLocalAccountIdentification.cs
+++ Adyen/Transfers/Models/DKLocalAccountIdentification.cs
@@ -25,6 +25,7 @@ using System.Text.Json;
 using System.Text.Json.Serialization;
 using System.ComponentModel.DataAnnotations;
 using Adyen.Core;
+using Adyen.Core.Converters;
 using Adyen.Transfers.Client;
 
 namespace Adyen.Transfers.Models
@@ -32,6 +33,7 @@ namespace Adyen.Transfers.Models
     /// <summary>
     /// DKLocalAccountIdentification.
     /// </summary>
+    [JsonConverter(typeof(DKLocalAccountIdentificationJsonConverter))]
     public partial class DKLocalAccountIdentification
     {
         /// <summary>
--- Adyen/Transfers/Models/TransferNotificationCounterParty.cs
+++ Adyen/Transfers/Models/TransferNotificationCounterParty.cs
@@ -25,6 +25,7 @@ using System.Text.Json;
 using System.Text.Json.Serialization;
 using System.ComponentModel.DataAnnotations;
 using Adyen.Core;
+using Adyen.Core.Converters;
 using Adyen.Transfers.Client;
 
 namespace Adyen.Transfers.Models
@@ -32,6 +33,7 @@ namespace Adyen.Transfers.Models
     /// <summary>
     /// TransferNotificationCounterParty.
     /// </summary>
+    [JsonConverter(typeof(TransferNotificationCounterPartyJsonConverter))]
     public partial class TransferNotificationCounterParty
     {
         /// <summary>
