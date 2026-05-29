9bfbb730e5bc3b608b3c2487df1bcfdaf3a27460
AdyenAutomationBot <38424300+AdyenAutomationBot@users.noreply.github.com>
2026-05-29 13:48:59 +0000

    [disputewebhooks] Automated update from Adyen/adyen-openapi@1e3919b

--- Adyen/DisputeWebhooks/Models/DisputeEventNotification.cs
+++ Adyen/DisputeWebhooks/Models/DisputeEventNotification.cs
@@ -25,6 +25,7 @@ using System.Text.Json;
 using System.Text.Json.Serialization;
 using System.ComponentModel.DataAnnotations;
 using Adyen.Core;
+using Adyen.Core.Converters;
 using Adyen.DisputeWebhooks.Client;
 
 namespace Adyen.DisputeWebhooks.Models
@@ -32,6 +33,7 @@ namespace Adyen.DisputeWebhooks.Models
     /// <summary>
     /// DisputeEventNotification.
     /// </summary>
+    [JsonConverter(typeof(DisputeEventNotificationJsonConverter))]
     public partial class DisputeEventNotification
     {
         /// <summary>
