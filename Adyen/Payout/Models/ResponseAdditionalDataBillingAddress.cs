222a5ef7e9eb3add6c4d57f2bbc1a611164af4c0
AdyenAutomationBot <38424300+AdyenAutomationBot@users.noreply.github.com>
2026-05-29 13:48:58 +0000

    [payout] Automated update from Adyen/adyen-openapi@1e3919b

--- Adyen/Payout/Models/ResponseAdditionalDataBillingAddress.cs
+++ Adyen/Payout/Models/ResponseAdditionalDataBillingAddress.cs
@@ -25,6 +25,7 @@ using System.Text.Json;
 using System.Text.Json.Serialization;
 using System.ComponentModel.DataAnnotations;
 using Adyen.Core;
+using Adyen.Core.Converters;
 using Adyen.Payout.Client;
 
 namespace Adyen.Payout.Models
@@ -32,6 +33,7 @@ namespace Adyen.Payout.Models
     /// <summary>
     /// ResponseAdditionalDataBillingAddress.
     /// </summary>
+    [JsonConverter(typeof(ResponseAdditionalDataBillingAddressJsonConverter))]
     public partial class ResponseAdditionalDataBillingAddress
     {
         /// <summary>
