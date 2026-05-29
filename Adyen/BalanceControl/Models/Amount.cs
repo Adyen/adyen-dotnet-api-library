71a03e7f90822b2bc93ed6772c6f4d37da08dc0c
AdyenAutomationBot <38424300+AdyenAutomationBot@users.noreply.github.com>
2026-05-29 13:49:04 +0000

    [balancecontrol] Automated update from Adyen/adyen-openapi@1e3919b

--- Adyen/BalanceControl/Models/Amount.cs
+++ Adyen/BalanceControl/Models/Amount.cs
@@ -25,6 +25,7 @@ using System.Text.Json;
 using System.Text.Json.Serialization;
 using System.ComponentModel.DataAnnotations;
 using Adyen.Core;
+using Adyen.Core.Converters;
 using Adyen.BalanceControl.Client;
 
 namespace Adyen.BalanceControl.Models
@@ -32,6 +33,7 @@ namespace Adyen.BalanceControl.Models
     /// <summary>
     /// Amount.
     /// </summary>
+    [JsonConverter(typeof(AmountJsonConverter))]
     public partial class Amount
     {
         /// <summary>
