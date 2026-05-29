70f29ba7d365d320aa3e3573940c84205a1abf50
AdyenAutomationBot <38424300+AdyenAutomationBot@users.noreply.github.com>
2026-05-29 13:49:01 +0000

    [posmobile] Automated update from Adyen/adyen-openapi@1e3919b

--- Adyen/PosMobile/Models/CreateSessionRequest.cs
+++ Adyen/PosMobile/Models/CreateSessionRequest.cs
@@ -25,6 +25,7 @@ using System.Text.Json;
 using System.Text.Json.Serialization;
 using System.ComponentModel.DataAnnotations;
 using Adyen.Core;
+using Adyen.Core.Converters;
 using Adyen.PosMobile.Client;
 
 namespace Adyen.PosMobile.Models
@@ -32,6 +33,7 @@ namespace Adyen.PosMobile.Models
     /// <summary>
     /// CreateSessionRequest.
     /// </summary>
+    [JsonConverter(typeof(CreateSessionRequestJsonConverter))]
     public partial class CreateSessionRequest
     {
         /// <summary>
